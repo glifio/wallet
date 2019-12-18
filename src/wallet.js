53ff87207fba8a696ca6494c7ef4e9074afacf6a
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-12-18 18:28:09 -0500

    pass token through to filecoin provider

--- src/wallet.js
+++ src/wallet.js
@@ -4,5 +4,6 @@ export default new Filecoin(
   new LocalNodeProvider({
     apiAddress: 'https://lotus-dev.temporal.cloud/rpc/v0',
     token: process.env.REACT_APP_LOTUS_JWT_TOKEN,
-  })
+  }),
+  { token: process.env.REACT_APP_LOTUS_JWT_TOKEN }
 );
