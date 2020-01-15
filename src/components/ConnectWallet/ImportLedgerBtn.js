e83dffd680dfb92cbe0138cde92fd8b01576fc8e
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-01-15 08:57:42 -0500

    improve ledger onboarding and error handling

--- src/components/ConnectWallet/ImportLedgerBtn.js
+++ src/components/ConnectWallet/ImportLedgerBtn.js
@@ -2,30 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import 'styled-components/macro'
 import { useSelector } from 'react-redux'
-import Filecoin, {
-  LedgerProvider
-} from '@openworklabs/filecoin-wallet-provider'
 
-import {
-  error,
-  walletList,
-  createWalletProvider,
-  clearError
-} from '../../store/actions'
 import { useProgress } from '../../hooks'
-import {
-  USER_INITIATED_IMPORT,
-  LEDGER_NOT_FOUND,
-  ESTABLISHING_CONNECTION_W_FILECOIN_APP,
-  FILECOIN_APP_NOT_OPEN,
-  FILECOIN_APP_OPEN,
-  LEDGER_LOCKED,
-  LEDGER_UNLOCKED,
-  LEDGER_CONNECTED,
-  LEDGER_STATE_PROPTYPES
-} from './ledgerStateManagement'
+import { LEDGER_STATE_PROPTYPES } from './ledgerStateManagement'
 import { Button } from './styledComponents'
-import createTransport from './transport'
+import connectLedger from './connectLedger'
 
 const ImportLedgerBtn = ({ ledgerState, dispatchRdx, dispatchLocal }) => {
   const { setProgress } = useProgress()
@@ -39,68 +20,14 @@ const ImportLedgerBtn = ({ ledgerState, dispatchRdx, dispatchLocal }) => {
           ledgerState.userVerifiedFilecoinAppOpen
         )
       }
-      onClick={async () => {
-        dispatchLocal({ type: USER_INITIATED_IMPORT })
-        let transport = ledgerState.transport
-        try {
-          if (!transport) {
-            transport = await createTransport()
-          }
-          dispatchLocal({ type: LEDGER_CONNECTED, transport })
-        } catch (err) {
-          if (
-            err.message &&
-            !err.message.toLowerCase().includes('device is already open')
-          ) {
-            dispatchLocal({ type: LEDGER_NOT_FOUND })
-            // if we want to display banner instead:
-            dispatchRdx(error(err))
-            return
-          }
-        }
-
-        dispatchLocal({ type: ESTABLISHING_CONNECTION_W_FILECOIN_APP })
-        let provider = walletProvider
-        try {
-          if (!provider) {
-            provider = new Filecoin(new LedgerProvider(transport), {
-              token: process.env.REACT_APP_LOTUS_JWT_TOKEN
-            })
-          }
-          const response = await provider.wallet.getVersion()
-          if (response.device_locked) {
-            dispatchLocal({ type: LEDGER_LOCKED })
-            dispatchRdx(error(new Error('Ledger device locked')))
-            return
-          }
-
-          dispatchLocal({ type: LEDGER_UNLOCKED })
-          dispatchLocal({ type: FILECOIN_APP_OPEN })
-          dispatchRdx(createWalletProvider(provider))
-        } catch (err) {
-          dispatchLocal({ type: FILECOIN_APP_NOT_OPEN })
-          dispatchRdx(error(err))
-          return
-        }
-
-        try {
-          const filAddresses = await provider.wallet.getAccounts(0, 1)
-          const wallets = await Promise.all(
-            filAddresses.map(async address => {
-              const balance = await provider.getBalance(address)
-              return {
-                balance,
-                address
-              }
-            })
-          )
-          dispatchRdx(walletList(wallets))
-          dispatchRdx(clearError())
-          setProgress(2)
-        } catch (err) {
-          dispatchRdx(error(err))
-        }
-      }}
+      onClick={async () =>
+        await connectLedger(
+          walletProvider,
+          setProgress,
+          dispatchLocal,
+          dispatchRdx
+        )
+      }
     >
       {ledgerState.userImportFailure ? 'Try again' : 'Import Ledger Wallet'}
     </Button>
--- src/components/ConnectWallet/transport.js
+++ src/components/ConnectWallet/transport.js
@@ -1,16 +1,16 @@
 import TransportWebHID from '@ledgerhq/hw-transport-webhid'
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb'
 
-export default async () => {
-  let transport
-
-  try {
-    transport = await TransportWebUSB.create()
-    return transport
-  } catch (_) {}
-  try {
-    transport = await TransportWebHID.create()
-    return transport
-  } catch (_) {}
-  throw new Error('No successful transport established')
-}
+export default () =>
+  new Promise((resolve, reject) => {
+    TransportWebHID.create()
+      .then(resolve)
+      .catch(err => {
+        if (err.message.includes('navigator.hid is not supported')) {
+          return TransportWebUSB.create()
+        }
+        reject(err)
+      })
+      .then(resolve)
+      .catch(reject)
+  })
