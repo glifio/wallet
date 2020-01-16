9232a132e8886b38af8981da4473b179033e4831
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-01-16 09:46:57 -0500

    add copy address to clipboard

--- src/components/DisplayWallet/AccountPicker.js
+++ src/components/DisplayWallet/AccountPicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 import { useWallets } from '../../hooks'
 import {
@@ -7,24 +7,42 @@ import {
   SwitchAccountButton,
   AccountHeader,
   AccountDetail,
-  AccountAddress
+  AccountAddress,
+  JustifyContentContainer
 } from '../StyledComponents'
+
+import CopyToClipboardIcon from './ClipboardIcon'
+import { copyToClipboard } from '../../utils'
 
 export default () => {
   const { selectedWallet } = useWallets()
+  const [copiedToClipboard, setCopiedToClipboard] = useState(false)
+
+  const onClick = useCallback(() => {
+    setCopiedToClipboard(true)
+    copyToClipboard(selectedWallet.address)
+  }, [selectedWallet.address])
 
   return (
-    <React.Fragment>
-      <AccountHeader>
-        <AccountLabel>Account</AccountLabel>
-        <AccountDetail>
-          <AccountAddress>{selectedWallet.address}</AccountAddress>
-          <AccountBalance>
-            {selectedWallet.balance.toString()} FIL
-          </AccountBalance>
-        </AccountDetail>
-        <SwitchAccountButton>Switch account</SwitchAccountButton>
-      </AccountHeader>
-    </React.Fragment>
+    <AccountHeader>
+      <AccountLabel>Account</AccountLabel>
+      <AccountDetail>
+        <JustifyContentContainer
+          flexDirection='row'
+          justifyContent='space-between'
+        >
+          <AccountAddress role='button' onClick={onClick}>
+            {selectedWallet.address}
+          </AccountAddress>
+          {copiedToClipboard ? (
+            <span>&#10003;</span>
+          ) : (
+            <CopyToClipboardIcon onClick={onClick} />
+          )}
+        </JustifyContentContainer>
+        <AccountBalance>{selectedWallet.balance.toString()} FIL</AccountBalance>
+      </AccountDetail>
+      <SwitchAccountButton>Switch account</SwitchAccountButton>
+    </AccountHeader>
   )
 }
