258db66152021d9e36f6daa5393600b15010220f
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-01-16 10:42:42 -0500

    fix signature from ledger

--- src/components/DisplayWallet/computeSig.js
+++ src/components/DisplayWallet/computeSig.js
@@ -3,12 +3,11 @@ import { LEDGER } from '../../constants'
 export default async (provider, selectedWallet, message, walletType) => {
   if (walletType === LEDGER) {
     const serializedMessage = await message.serialize()
-    const { signature, error_message } = await provider.wallet.sign(
+    const signature = await provider.wallet.sign(
       selectedWallet.path,
       serializedMessage
     )
-    if (error_message.toLowerCase() === 'no errors') return signature
-    throw new Error(error_message)
+    return signature
   }
   throw new Error('Unsupported wallet type')
 }
