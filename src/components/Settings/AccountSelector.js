82cf8bcb774148d93a719811284ecb7fb8593184
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-01-23 10:52:32 -0500

    Improve ledger handling in settings

--- src/components/Settings/AccountSelector.js
+++ src/components/Settings/AccountSelector.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import 'styled-components/macro'
+
 import { useSelector, useDispatch } from 'react-redux'
 import { useLocation, useHistory } from 'react-router-dom'
 
@@ -10,12 +12,20 @@ import {
   Checkbox,
   CheckboxInputLabel,
   BASE_SIZE_UNIT,
-  TEXT_XSM
+  TEXT_XSM,
+  UnderlineOnHover
 } from '../StyledComponents'
 
 import { walletList, switchWallet, error } from '../../store/actions'
 import sortAndRemoveWalletDups from './sortAndRemoveWalletDups'
 import { ACCOUNT_BATCH_SIZE } from '../../constants'
+import {
+  fetchProvider,
+  reducer,
+  initialLedgerState,
+  RESET_STATE
+} from '../../utils/ledger'
+import { LEDGER } from '../../constants'
 
 const ButtonContainer = styled(JustifyContentContainer)`
   justify-self: flex-end;
@@ -34,9 +44,12 @@ const LineItem = styled(JustifyContentContainer)`
   align-items: center;
 `
 
-const LoadingText = styled.p`
+const Text = styled.p`
   font-weight: bold;
   font-size: ${TEXT_XSM}px;
+`
+
+const LoadingText = styled(Text)`
   line-height: 20;
 `
 
@@ -50,44 +63,56 @@ const AccountSelector = ({
   const params = new URLSearchParams(search)
   const page = Number(params.get('page'))
   const history = useHistory()
-  const { walletProvider, selectedWallet, walletsInRdx } = useSelector(
-    state => ({
-      walletProvider: state.walletProvider,
-      selectedWallet: state.wallets[state.selectedWalletIdx],
-      walletsInRdx: state.wallets
-    })
-  )
+  const {
+    walletProvider,
+    selectedWallet,
+    walletsInRdx,
+    walletType
+  } = useSelector(state => ({
+    walletProvider: state.walletProvider,
+    selectedWallet: state.wallets[state.selectedWalletIdx],
+    walletsInRdx: state.wallets,
+    walletType: state.walletType
+  }))
 
   const dispatch = useDispatch()
+
+  const [ledgerState, dispatchLocal] = useReducer(reducer, initialLedgerState)
 
   useEffect(() => {
     const fetchAccounts = async () => {
       setLoadingAccounts(true)
       try {
-        const accounts = await walletProvider.wallet.getAccounts(
-          page * ACCOUNT_BATCH_SIZE,
-          page * ACCOUNT_BATCH_SIZE + ACCOUNT_BATCH_SIZE,
-          network
-        )
+        let provider = walletProvider
+        if (walletType === LEDGER) {
+          provider = await fetchProvider(dispatchLocal, dispatch)
+        }
+        if (provider) {
+          const accounts = await provider.wallet.getAccounts(
+            page * ACCOUNT_BATCH_SIZE,
+            page * ACCOUNT_BATCH_SIZE + ACCOUNT_BATCH_SIZE,
+            network
+          )
 
-        const wallets = await Promise.all(
-          accounts.map(async (address, i) => {
-            const balance = await walletProvider.getBalance(address)
-            const networkDerivationPath = network === 'f' ? 461 : 1
-            return {
-              balance,
-              address,
-              path: [
-                44,
-                networkDerivationPath,
-                5,
-                0,
-                page * ACCOUNT_BATCH_SIZE + i
-              ]
-            }
-          })
-        )
-        dispatch(walletList(sortAndRemoveWalletDups(walletsInRdx, wallets)))
+          const wallets = await Promise.all(
+            accounts.map(async (address, i) => {
+              const balance = await provider.getBalance(address)
+              const networkDerivationPath = network === 'f' ? 461 : 1
+              return {
+                balance,
+                address,
+                path: [
+                  44,
+                  networkDerivationPath,
+                  5,
+                  0,
+                  page * ACCOUNT_BATCH_SIZE + i
+                ]
+              }
+            })
+          )
+          dispatch(walletList(sortAndRemoveWalletDups(walletsInRdx, wallets)))
+        }
       } catch (err) {
         dispatch(error(err))
       }
@@ -111,9 +136,16 @@ const AccountSelector = ({
       return matchCount < ACCOUNT_BATCH_SIZE
     }
 
-    if (tabOpen && !loadingAccounts && needToFetch()) fetchAccounts()
+    if (
+      tabOpen &&
+      !loadingAccounts &&
+      !ledgerState.userImportFailure &&
+      needToFetch()
+    )
+      fetchAccounts()
   }, [
     loadingAccounts,
+    ledgerState.userImportFailure,
     tabOpen,
     walletsInRdx.length,
     dispatch,
@@ -121,7 +153,8 @@ const AccountSelector = ({
     page,
     setLoadingAccounts,
     walletProvider,
-    walletsInRdx
+    walletsInRdx,
+    walletType
   ])
 
   const paginate = page => {
@@ -134,9 +167,34 @@ const AccountSelector = ({
   return (
     <SettingsContainer flexDirection='column' justifyContent='space-between'>
       <div>
-        {loadingAccounts ? (
+        {ledgerState.userInitiatedImport && ledgerState.userImportFailure ? (
           <JustifyContentContainer flexDirection='row' justifyContent='center'>
-            <LoadingText>Loading...</LoadingText>
+            <JustifyContentContainer
+              flexDirection='column'
+              justifyContent='center'
+              css={{ 'margin-top': '20%' }}
+            >
+              <Text>
+                Is your Ledger plugged in, unlocked, and Filecoin app open?
+              </Text>
+              <UnderlineOnHover
+                role='button'
+                rel='noopener noreferrer'
+                onClick={() => dispatchLocal({ type: RESET_STATE })}
+              >
+                Yes.
+              </UnderlineOnHover>
+            </JustifyContentContainer>
+          </JustifyContentContainer>
+        ) : loadingAccounts ? (
+          <JustifyContentContainer flexDirection='row' justifyContent='center'>
+            <LoadingText
+              css={`
+                line-height: 20;
+              `}
+            >
+              Loading...
+            </LoadingText>
           </JustifyContentContainer>
         ) : (
           walletsInRdx
@@ -176,13 +234,15 @@ const AccountSelector = ({
       </div>
       <ButtonContainer flexDirection='row' justifyContent='space-around'>
         <ButtonBase
-          disabled={loadingAccounts || page === 0}
+          disabled={
+            ledgerState.userImportFailure || loadingAccounts || page === 0
+          }
           onClick={() => paginate(page - 1)}
         >
           Previous
         </ButtonBase>
         <ButtonBase
-          disabled={loadingAccounts}
+          disabled={ledgerState.userImportFailure || loadingAccounts}
           onClick={() => paginate(page + 1)}
         >
           Next
