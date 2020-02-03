c3fed2246b871d691ef73c64486bd135488b5b36
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-03 12:02:43 -0500

    Add faq content

--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -23,18 +23,58 @@ export default () => (
     <Accordion defaultActiveKey='0'>
       <Card>
         <Accordion.Toggle as={Card.Header} eventKey='0'>
-          <Question>Click me!</Question>
+          <Question>What is a wallet?</Question>
         </Accordion.Toggle>
         <Accordion.Collapse eventKey='0'>
-          <Card.Body>Hello! I'm the body</Card.Body>
+          <Card.Body>
+            A wallet is a public/private key pair. Your private key is a secret,
+            it should be kept safe and secure in a place where no one else can
+            access it. Your public key is available for anyone to see on the
+            network. Your wallet’s address (starts with the letter “t” or “f”)
+            is what you see on this app - it’s a reformatted version of your
+            public key and is the information you should give anyone else trying
+            to send you Filecoin.
+          </Card.Body>
         </Accordion.Collapse>
       </Card>
       <Card>
-        <Accordion.Toggle as={Card.Header} eventKey='1'>
-          <Question>Click me!</Question>
+        <Accordion.Toggle as={Card.Header} eventKey='2'>
+          <Question>Does this site store my private keys?</Question>
         </Accordion.Toggle>
-        <Accordion.Collapse eventKey='1'>
-          <Card.Body>Hello! I'm another body</Card.Body>
+        <Accordion.Collapse eventKey='2'>
+          <Card.Body>
+            We do not store any information about your private keys. Wallets can
+            be imported or created for each session, but as soon as you refresh
+            the page or leave your computer idol, we erase your private keys
+            from memory. It is your responsibility to store keys in a safe and
+            secure location.
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey='3'>
+          <Question>How can I create additional wallets?</Question>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey='3'>
+          <Card.Body>
+            In the top right corner, select the accounts/network button to
+            switch wallets. If you don’t see any other options, it’s because you
+            chose to import a single wallet into our app.
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey='4'>
+          <Question>
+            What do I do if I lost my seed phrase or private key?
+          </Question>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey='4'>
+          <Card.Body>
+            We’re really sorry, but there isn’t much we can help with if you
+            lose your seed phrase or private key. For security reasons, we do
+            not copy your private information or back it up anywhere.
+          </Card.Body>
         </Accordion.Collapse>
       </Card>
     </Accordion>
@@ -43,7 +83,7 @@ export default () => (
         margin-top: 15px;
       `}
     />
-    <Accordion>
+    {/* <Accordion>
       <p>
         <strong>The legalities</strong>
       </p>
@@ -63,6 +103,6 @@ export default () => (
           <Card.Body>Hello! I'm another body</Card.Body>
         </Accordion.Collapse>
       </Card>
-    </Accordion>
+    </Accordion> */}
   </Container>
 )
