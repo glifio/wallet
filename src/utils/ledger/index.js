2866011d43d01ba01b22f253e5c51b6afa878b97
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-04 09:06:49 -0500

    add apiAddress to Filecoin provider instantiation

--- src/utils/ledger/index.js
+++ src/utils/ledger/index.js
@@ -46,7 +46,8 @@ export const establishConnectionWithFilecoinApp = async (
   dispatchLocal({ type: ESTABLISHING_CONNECTION_W_FILECOIN_APP })
   try {
     const provider = new Filecoin(new LedgerProvider(transport), {
-      token: process.env.REACT_APP_LOTUS_JWT_TOKEN
+      token: process.env.REACT_APP_LOTUS_JWT_TOKEN,
+      apiAddress: process.env.REACT_APP_LOTUS_API
     })
     const response = await provider.wallet.getVersion()
 
