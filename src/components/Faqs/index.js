7dd25a5b9eab39723c614aa8915b1ec84cc3c5e3
listenaddress <tbobrien612@gmail.com>
2020-02-05 16:40:02 -0800

    Add FAQ for where to get FIL

--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -38,10 +38,10 @@ export default () => (
         </Accordion.Collapse>
       </Card>
       <Card>
-        <Accordion.Toggle as={Card.Header} eventKey='2'>
+        <Accordion.Toggle as={Card.Header} eventKey='1'>
           <Question>Does this site store my private keys?</Question>
         </Accordion.Toggle>
-        <Accordion.Collapse eventKey='2'>
+        <Accordion.Collapse eventKey='1'>
           <Card.Body>
             We do not store any information about your private keys. Wallets can
             be imported or created for each session, but as soon as you refresh
@@ -52,10 +52,10 @@ export default () => (
         </Accordion.Collapse>
       </Card>
       <Card>
-        <Accordion.Toggle as={Card.Header} eventKey='3'>
+        <Accordion.Toggle as={Card.Header} eventKey='2'>
           <Question>How can I create additional wallets?</Question>
         </Accordion.Toggle>
-        <Accordion.Collapse eventKey='3'>
+        <Accordion.Collapse eventKey='2'>
           <Card.Body>
             In the top right corner, select the accounts/network button to
             switch wallets. If you don’t see any other options, it’s because you
@@ -64,16 +64,31 @@ export default () => (
         </Accordion.Collapse>
       </Card>
       <Card>
-        <Accordion.Toggle as={Card.Header} eventKey='4'>
+        <Accordion.Toggle as={Card.Header} eventKey='3'>
           <Question>
             What do I do if I lost my seed phrase or private key?
           </Question>
         </Accordion.Toggle>
-        <Accordion.Collapse eventKey='4'>
+        <Accordion.Collapse eventKey='3'>
           <Card.Body>
             We’re really sorry, but there isn’t much we can help with if you
             lose your seed phrase or private key. For security reasons, we do
             not copy your private information or back it up anywhere.
+          </Card.Body>
+        </Accordion.Collapse>
+        <Accordion.Toggle as={Card.Header} eventKey='4'>
+          <Question>Where can I get Filecoin?</Question>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey='4'>
+          <Card.Body>
+            You can't buy Filecoin here right now, but after mainnet launch
+            you'll be able to buy Filecoin from a number of exchanges.
+            <br />
+            <br />
+            <div>
+              If you're using the testnet, you can get funny Filecoin from a{' '}
+              <a href='https://faucet.testnet.filecoin.io/'>faucet</a>.
+            </div>
           </Card.Body>
         </Accordion.Collapse>
       </Card>
