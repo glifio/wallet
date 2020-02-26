c559e6ef596c26ca0506c6dfccb6debf544103ca
Alex Singh <alex@alexsingh.com>
2020-02-26 14:14:18 -0300

    Update text colors and removed border

--- components/Shared/MessageHistoryTable/EmptyHistory.jsx
+++ components/Shared/MessageHistoryTable/EmptyHistory.jsx
@@ -4,11 +4,11 @@ import { Text } from '../Typography'
 
 export default () => {
   return (
-    <Box border={1} p={2}>
-      <Text ml={2} my='0' color='core.darkgray'>
+    <Box>
+      <Text ml={2} my='0' color='core.nearblack'>
         How do I see my transaction history?
       </Text>
-      <Text ml={2} my='0' color='core.lightgray'>
+      <Text ml={2} my='0' color='core.darkgray'>
         If you're seeing this, you haven't sent or received any FIL from this
         account yet. When you do, your transactions will appear.
       </Text>
