2bf1dec3d40091917395e1105c18b215fd6718d9
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-26 12:16:21 -0500

    Remove mock state

--- store/states.js
+++ store/states.js
@@ -10,54 +10,8 @@ export const initialState = {
     loading: false,
     loadedSuccess: false,
     loadedFailure: false,
-    pending: [
-      {
-        cid: 'bafy2bzaceaa3pmye6ralr6ekgsijt2s36dihs3de3g4ccwstrlpl325x45go4',
-        from: 't1hn7twanih6djfrg7s3phaek3ayge72c6vhndrhq',
-        to: 't1jdlfl73voaiblrvn2yfivvn5ifucwwv5f26nfza',
-        nonce: '23344',
-        value: '5000000000000000',
-        gasprice: '0',
-        gaslimit: '1000',
-        method: '0',
-        gas_used: 196
-      },
-      {
-        cid: 'bafy2bzaceamz3qggtumnbqkgyldhga54y3zakvrt2deuedwkz7fk5nrsmasrs',
-        from: 't1hn7twanih6djfrg7s3phaek3ayge72c6vhndrhq',
-        to: 't1jdlfl73voaiblrvn2yfivvn5ifucwwv5f26nfza',
-        nonce: '23344',
-        value: '5000000000000000',
-        gasprice: '0',
-        gaslimit: '1000',
-        method: '0',
-        gas_used: 196
-      }
-    ],
-    confirmed: [
-      {
-        cid: 'bafy2bzaceabmpnqlbfgnkc6co72mhb36v37re2loikrdldlpmwharlajma2ug',
-        from: 't1jdlfl73voaiblrvn2yfivvn5ifucwwv5f26nfza',
-        to: 't1hn7twanih6djfrg7s3phaek3ayge72c6vhndrhq',
-        nonce: '21349',
-        value: '5000000000000000',
-        gasprice: '0',
-        gaslimit: '1000',
-        method: '0',
-        gas_used: 196
-      },
-      {
-        cid: 'bafy2bzaceabgvkl5rum5u7ctkwcmpo6ursryfyhgod35ewhoppyrhwrasdzty',
-        from: 't1hn7twanih6djfrg7s3phaek3ayge72c6vhndrhq',
-        to: 't1jdlfl73voaiblrvn2yfivvn5ifucwwv5f26nfza',
-        nonce: '21349',
-        value: '5000000000000000',
-        gasprice: '0',
-        gaslimit: '1000',
-        method: '0',
-        gas_used: 196
-      }
-    ],
+    pending: [],
+    confirmed: [],
     links: {},
     paginating: false
   },
