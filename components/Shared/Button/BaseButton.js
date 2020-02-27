5507ba7ab7f2c6c90293b34245c638abdccfe87a
Alex Singh <alex@alexsingh.com>
2020-02-27 13:32:10 -0300

    Add 'button' object variant to theme file, replaces buttonStyles with styled-system 'variant' prop

--- components/Shared/Button/BaseButton.js
+++ components/Shared/Button/BaseButton.js
@@ -1,17 +1,20 @@
 import styled from 'styled-components'
-import { space, layout, borderRadius, flexbox } from 'styled-system'
+import { space, layout, borderRadius, flexbox, variant } from 'styled-system'
 
 export default styled.button`
+  variant {
+    scale: 'buttons';
+  }
   cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
   background-color: ${props =>
     props.disabled
       ? props.theme.colors.status.inactive
-      : props.theme.colors.buttons[props.buttonStyle].background};
+      : props.theme.colors.buttons[props.variant].background};
   border-color: ${props =>
     props.disabled
       ? props.theme.colors.status.inactive
-      : props.theme.colors.buttons[props.buttonStyle].borderColor};
-  color: ${props => props.theme.colors.buttons[props.buttonStyle].color};
+      : props.theme.colors.buttons[props.variant].borderColor};
+  color: ${props => props.theme.colors.buttons[props.variant].color};
   font-size: ${props => props.theme.fontSizes[2]}; 
   ${borderRadius} 
   ${space}
--- components/Shared/Button/index.jsx
+++ components/Shared/Button/index.jsx
@@ -1,36 +1,36 @@
 import React, { forwardRef } from 'react'
+import { variant } from 'styled-system'
 import { func, bool, string, oneOf } from 'prop-types'
 import BaseButton from './BaseButton'
 import theme from '../theme'
 
-const Button = forwardRef(
-  ({ buttonStyle, disabled, onClick, title, ...props }, ref) => (
-    <BaseButton
-      buttonStyle={buttonStyle}
-      p={3}
-      fontSize={3}
-      borderRadius={2}
-      onClick={onClick}
-      disabled={disabled}
-      ref={ref}
-      {...props}
-    >
-      {title}
-    </BaseButton>
-  )
-)
+const Button = forwardRef(({ disabled, onClick, title, ...props }, ref) => (
+  <BaseButton
+    variant={variant}
+    // buttonStyle={buttonStyle}
+    p={3}
+    fontSize={3}
+    borderRadius={2}
+    onClick={onClick}
+    disabled={disabled}
+    ref={ref}
+    {...props}
+  >
+    {title}
+  </BaseButton>
+))
 
-const ButtonPropTypes = {
-  buttonStyle: oneOf(Object.keys(theme.colors.buttons)),
+Button.propTypes = {
+  variant: oneOf(Object.keys(theme.buttons)),
+  // buttonStyle: oneOf(Object.keys(theme.colors.buttons)),
   onClick: func.isRequired,
   title: string.isRequired,
   disabled: bool
 }
 
-Button.propTypes = ButtonPropTypes
-
 Button.defaultProps = {
-  buttonStyle: 'primary'
+  variant: 'primary'
+  // buttonStyle: 'primary'
 }
 
 export default Button
