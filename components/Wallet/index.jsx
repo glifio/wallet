63f8e30265c9588307269c938b109c2835c3803a
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-27 08:55:47 -0500

    Add balance polling

--- components/Wallet/index.jsx
+++ components/Wallet/index.jsx
@@ -11,8 +11,10 @@ import {
   reportLedgerConfigError
 } from '../../utils/ledger/reportLedgerConfigError'
 import MsgConfirmer from '../../lib/confirm-message'
+import useUpToDateBalance from '../../lib/update-balance'
 
 const WalletView = ({ wallet }) => {
+  useUpToDateBalance()
   const [sending, setSending] = useState(false)
   const { ledger, walletType, connectLedger } = useWalletProvider()
   const [uncaughtError, setUncaughtError] = useState(null)
