e0e7656d73a267bb1c4a4797afae0e26c7fc6de9
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-28 12:54:56 -0500

    Fix font loading

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,12 @@
 import App from 'next/app'
+import Router from 'next/router'
 import React from 'react'
-import { Provider } from 'react-redux'
 import { createGlobalStyle } from 'styled-components'
+import { Provider } from 'react-redux'
 import { theme, ThemeProvider } from '../components/Shared'
 import withReduxStore from '../lib/with-redux-store'
 import WalletProviderWrapper from '../WalletProvider'
-import { switchNetwork } from '../store/actions'
+import NetworkChecker from '../lib/check-network'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -17,60 +18,66 @@ const GlobalStyle = createGlobalStyle`
   /* Alias Medium */
 
   @font-face {
-  font-family: 'RT-Alias-Medium';
-  src: url('fonts/RTAliasMedium-Light.woff2');
-  font-weight: 200;
- }
+    font-family: 'RT-Alias-Medium';
+    src: url('/static/fonts/RTAliasMedium-Light.woff2');
+    font-weight: 200;
+  }
 
- @font-face {
-  font-family: 'RT-Alias-Medium';
-  src: url('fonts/RTAliasMedium-Regular.woff2');
-  font-weight: 400;
- }
   @font-face {
-  font-family: 'RT-Alias-Medium';
-  src: url('fonts/RTAliasMedium-Bold.woff2');
-  font-weight: 700;
- }
+    font-family: 'RT-Alias-Medium';
+    src: url('/static/fonts/RTAliasMedium-Regular.woff2');
+    font-weight: 400;
+  }
 
-/* Alias Grotesk */
-
- @font-face {
-  font-family: 'RT-Alias-Grotesk';
-  src: url('fonts/RTAliasGrotesk-Light.woff2');
-  font-weight: 200;
- }
-
- @font-face {
-  font-family: 'RT-Alias-Grotesk';
-  src: url('fonts/RTAliasGrotesk-Regular.woff2');
-  font-weight: 400;
- }
   @font-face {
-  font-family: 'RT-Alias-Grotesk';
-  src: url('fonts/RTAliasGrotesk-Bold.woff2');
-  font-weight: 700;
- }
- 
+    font-family: 'RT-Alias-Medium';
+    src: url('/static/fonts/RTAliasMedium-Bold.woff2');
+    font-weight: 700;
+  }
+
+  /* Alias Grotesk */
+
+  @font-face {
+    font-family: 'RT-Alias-Grotesk';
+    src: url('/static/fonts/RTAliasGrotesk-Light.woff2');
+    font-weight: 200;
+  }
+
+  @font-face {
+    font-family: 'RT-Alias-Grotesk';
+    src: url('/static/fonts/RTAliasGrotesk-Regular.woff2');
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'RT-Alias-Grotesk';
+    src: url('/static/fonts/RTAliasGrotesk-Bold.woff2');
+    font-weight: 700;
+  }
 `
 
 class MyApp extends App {
-  static getInitialProps({ ctx: { query } }) {
+  static getInitialProps({ ctx: { query, pathname } }) {
     const clonedQuery = { ...query }
     if (!query.network) clonedQuery.network = 'f'
-    return { query: clonedQuery }
+    return { query: clonedQuery, pathname }
+  }
+
+  componentDidMount() {
+    const { reduxStore, query, pathname } = this.props
+    if (reduxStore.getState().network !== query.network) {
+      const params = new URLSearchParams(query)
+      Router.replace(`${pathname}?${params.toString()}`)
+    }
   }
 
   render() {
     const { Component, pageProps, reduxStore, query } = this.props
-
-    if (reduxStore.getState().network !== query.network) {
-      reduxStore.dispatch(switchNetwork(query.network))
-    }
     return (
       <Provider store={reduxStore}>
+        <GlobalStyle />
         <WalletProviderWrapper network={query.network}>
-          <GlobalStyle />
+          <NetworkChecker query={query} />
 
           <ThemeProvider theme={theme}>
             <Component {...pageProps} />
