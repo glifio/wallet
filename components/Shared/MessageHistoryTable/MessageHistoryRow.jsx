5d1dc06ed5c9698c1821d8b534bb2488a3d644d5
Jon <Schwartz10@users.noreply.github.com>
2020-03-01 18:04:59 -0500

    Use proper Filecoin denom in message history table (#139)

--- components/Shared/MessageHistoryTable/MessageHistoryRow.jsx
+++ components/Shared/MessageHistoryTable/MessageHistoryRow.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FilecoinNumber } from '@openworklabs/filecoin-number'
 import { bool, string } from 'prop-types'
 import { MESSAGE_PROPS, ADDRESS_PROPTYPE } from '../../../customPropTypes'
 import Box from '../Box'
@@ -126,12 +127,12 @@ const MessageHistoryRow = ({
           >
             <MenuItem display='flex'>
               <Text color='core.nearblack' m={0}>
-                {value}
+                {new FilecoinNumber(value, 'attofil').toFil()}
               </Text>
             </MenuItem>
             <MenuItem display='flex'>
               <Text color='core.silver' m={0} mb={0}>
-                {value}
+                {new FilecoinNumber(value, 'attofil').toFil()}
               </Text>
             </MenuItem>
           </Menu>
