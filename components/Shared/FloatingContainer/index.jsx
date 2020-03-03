eb128f2099d12e26cbaeba4fee628cb6a5aa181f
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-03-03 08:50:22 -0500

    Export FloatingContainer as a Shared component

--- components/Shared/FloatingContainer/index.jsx
+++ components/Shared/FloatingContainer/index.jsx
@@ -1,62 +1,19 @@
-import React from 'react'
 import styled from 'styled-components'
-import Button from './Button'
-import Text from './Typography'
-import Box from './Box'
+import Box from '../Box'
 
-const FloatingContainer = styled(Box)`
+export default styled(Box).attrs(() => ({
+  maxWidth: 13,
+  backgroundColor: 'core.white',
+  boxShadow: 2,
+  border: 1,
+  borderColor: 'core.silver',
+  p: 1
+}))`
   position: fixed;
   display: flex;
-  bottom: ${props => props.theme.sizes[3]}px;
   flex-direction: row;
+  bottom: ${props => props.theme.sizes[3]}px;
   justify-content: space-between;
   width: calc(58% - 1rem);
-  max-width: 580px;
-  box-shadow: ${props => props.theme.shadows[2]};
-  background-color: ${props => props.theme.colors.core.white};
-  border: ${props => props.theme.borders[1]};
-  border-color: ${props => props.theme.colors.core.silver};
   border-radius: ${props => props.theme.radii[2]};
 `
-
-const FloatingContainer = forwardRef(({ ...props }, ref) => (
-  <FloatingContainerWrapper {...props} ref={ref}>
-    {step === 2 && wallet.type === LEDGER ? (
-      <Text>Confirm or reject the transaction on your Ledger Device.</Text>
-    ) : (
-      <>
-        <Button
-          type='button'
-          title='Cancel'
-          variant='secondary'
-          onClick={() => {
-            setAttemptingTx(false)
-            setUncaughtError('')
-            resetLedgerState()
-            setSending(false)
-          }}
-        />
-        <Button
-          disabled={
-            !!(
-              hasError() ||
-              !isValidForm(
-                toAddress,
-                value.fil,
-                wallet.balance,
-                toAddressError,
-                valueError
-              )
-            )
-          }
-          type='submit'
-          title='Next'
-          variant='primary'
-          onClick={() => {}}
-        />
-      </>
-    )}
-  </FloatingContainerWrapper>
-))
-
-export default FloatingContainer
