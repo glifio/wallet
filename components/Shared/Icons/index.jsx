70eae399e6afd7776d88d288e7e3e64661d7c3dc
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-03-06 17:30:57 -0500

    Fix styling on Copy account icon

--- components/Shared/Icons/index.jsx
+++ components/Shared/Icons/index.jsx
@@ -250,6 +250,8 @@ export const IconApproximatelyEquals = forwardRef((props, ref) => (
 
 export const IconCopyAccountAddress = forwardRef((props, ref) => (
   <IconBase
+    width='24'
+    height='24'
     viewBox='0 0 24 24'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
