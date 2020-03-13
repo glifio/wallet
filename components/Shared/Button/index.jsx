6cf43b9c96488eaf65ce4f97c070e63c130ad0c5
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-03-13 12:18:41 -0400

    Improve button style flexibility

--- components/Shared/Button/index.jsx
+++ components/Shared/Button/index.jsx
@@ -1,33 +1,30 @@
 import React, { forwardRef } from 'react'
-import { variant } from 'styled-system'
 import { func, bool, string, oneOf } from 'prop-types'
 import BaseButton from './BaseButton'
 import theme from '../theme'
 
-const Button = forwardRef(({ disabled, onClick, title, ...props }, ref) => (
-  <BaseButton
-    variant={variant}
-    p={3}
-    fontSize={3}
-    borderRadius={2}
-    onClick={onClick}
-    disabled={disabled}
-    ref={ref}
-    {...props}
-  >
-    {title}
-  </BaseButton>
-))
+const Button = forwardRef(
+  ({ disabled, onClick, variant, title, ...props }, ref) => (
+    <BaseButton
+      variant={variant}
+      p={3}
+      fontSize={3}
+      borderRadius={2}
+      onClick={onClick}
+      disabled={disabled}
+      ref={ref}
+      {...props}
+    >
+      {title}
+    </BaseButton>
+  )
+)
 
 Button.propTypes = {
   variant: oneOf(Object.keys(theme.colors.buttons)),
   onClick: func.isRequired,
   title: string.isRequired,
   disabled: bool
-}
-
-Button.defaultProps = {
-  variant: 'primary'
 }
 
 export default Button
