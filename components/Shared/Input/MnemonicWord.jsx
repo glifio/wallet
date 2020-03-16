1a0e4798422526d6a893ed600766a5b589301067
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-03-16 16:54:41 -0400

    Work in progress commit

--- components/Shared/Input/MnemonicWord.jsx
+++ components/Shared/Input/MnemonicWord.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import {
   color,
@@ -8,8 +8,30 @@ import {
   flexbox,
   typography
 } from 'styled-system'
-import { func, string, bool } from 'prop-types'
+import { func, string, bool, number } from 'prop-types'
 import Box from '../Box'
+
+const setBackgroundColor = props => {
+  if (props.valid) return 'status.success.background'
+  if (props.fill) return 'core.primary'
+  return 'core.white'
+}
+
+const setInputColor = props => {
+  if (props.valid) return 'status.success.foreground'
+  if (props.value) return 'core.white'
+  return 'core.nearblack'
+}
+
+// &:focus {
+//   box-shadow: 0;
+//   outline: 0;
+//   background: ${props => {
+//     if (props.valid) return props.theme.colors.input.background.valid
+//     if (props.error) return props.theme.colors.input.background.invalid
+//     return props.theme.colors.core.primary
+//   }};
+// }
 
 export const MnemonicWordInput = styled.input.attrs(props => ({
   display: 'inline-block',
@@ -19,20 +41,12 @@ export const MnemonicWordInput = styled.input.attrs(props => ({
   backgroundColor: 'core.transparent',
   borderRadius: 6,
   border: 0,
-  outline: 0
+  outline: 0,
+  color: setInputColor(props)
 }))`
-  ::placeholder {
-    color: ${props => props.theme.colors.core.secondary};
-  }
-
   &:focus {
     box-shadow: 0;
     outline: 0;
-    background: ${props => {
-      if (props.valid) return props.theme.colors.input.background.valid
-      if (props.error) return props.theme.colors.input.background.invalid
-      return props.theme.colors.core.primary
-    }};
   }
   ${color}
   ${space}
@@ -42,24 +56,15 @@ export const MnemonicWordInput = styled.input.attrs(props => ({
   ${typography}
 `
 
-const MnemonicWord = ({
-  onChange,
-  value,
-  placeholder,
-  label,
-  error,
-  disabled,
-  valid,
-  num,
-  ...props
-}) => (
-  <>
+const MnemonicWord = ({ onChange, value, disabled, valid, num, ...props }) => {
+  const [fill, setFill] = useState(!!value)
+  return (
     <Box
       display='flex'
       alignItems='center'
       justifyContent='center'
       maxWidth={9}
-      backgroundColor='core.primary'
+      backgroundColor={setBackgroundColor({ valid, fill })}
       color='core.secondary'
       borderRadius={6}
       py={2}
@@ -80,41 +85,30 @@ const MnemonicWord = ({
         fontSize={3}
         borderRadius={6}
       >
-        {num || '1'}
+        {num}
       </Box>
       <MnemonicWordInput
-        label={label}
         onChange={onChange}
         value={value}
-        placeholder={placeholder}
-        error={error}
         valid={valid}
         {...props}
       />
     </Box>
-  </>
-)
+  )
+}
 
 MnemonicWord.propTypes = {
   onChange: func,
-  label: string,
-  setError: func,
   value: string,
-  error: string,
-  placeholder: string,
   valid: bool,
   disabled: bool,
-  numWord: string
+  num: number.isRequired
 }
 
 MnemonicWord.defaultProps = {
   value: '',
-  placeholder: 'tunafish',
   onChange: () => {},
-  setError: () => {},
-  label: '',
-  disabled: false,
-  numWord: ''
+  disabled: false
 }
 
 export default MnemonicWord
