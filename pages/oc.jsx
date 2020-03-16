1ded76ec12cc97cdf1f2792ac0ccce7bd250402c
Alex Singh <alex@alexsingh.com>
2020-03-16 15:26:47 -0300

    Temp. page to develop the MnemonicWordInput component & related elements

--- pages/oc.jsx
+++ pages/oc.jsx
@@ -1,6 +1,77 @@
 import React from 'react'
-import SeedPhrase from '../components/Shared/Input/SeedPhrase'
+import { Input, Menu, MenuItem, Box, Title } from '../components/Shared'
+
+const MnemonicObjectRow = () => {
+  return (
+    <Menu display='flex' flexWrap='wrap' justifyContent='space-around'>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+      <MenuItem>
+        <Input.MnemonicWord />
+      </MenuItem>
+    </Menu>
+  )
+}
 
 export default () => {
-  return <SeedPhrase />
+  return (
+    <Box
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      justifyItems='center'
+      maxWidth={16}
+      p={4}
+      border={1}
+      borderRadius={2}
+    >
+      <Box width='100%' textAlign='left' px={4} mb={4}>
+        <Title>Your Seed Phrase</Title>
+      </Box>
+      <Menu
+        display='flex'
+        alignItems='center'
+        justifyItems='center'
+        flexWrap='wrap'
+      >
+        <MenuItem>
+          <MnemonicObjectRow />
+        </MenuItem>
+        <MenuItem>
+          <MnemonicObjectRow />
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
 }
