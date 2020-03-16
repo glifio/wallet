0dadc66da23d08e4f046b6c35d14f099149f2046
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-03-16 09:18:10 -0400

    Use zondax's generate mnemonic method > bip39

--- WalletProvider/GenerateMnemonic.jsx
+++ WalletProvider/GenerateMnemonic.jsx
@@ -1,20 +1,19 @@
 import dynamic from 'next/dynamic'
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { generateMnemonic } from 'bip39'
 
 export default dynamic({
   ssr: false,
   loader: async () => {
     // Will use once we have generate mnemonic exposed from rust module
-    // const rustModule = await import('fcwebsigner')
+    const rustModule = await import('@zondax/filecoin-signer-wasm')
 
     const GenerateMnemonic = ({ setMnemonic }) => {
       const [createdMnemonic, setCreatedMnemonic] = useState(false)
 
       useEffect(() => {
         if (!createdMnemonic) {
-          setMnemonic(generateMnemonic())
+          setMnemonic(rustModule.mnemonic_generate())
           setCreatedMnemonic(true)
         }
       }, [createdMnemonic, setCreatedMnemonic, setMnemonic])
--- components/Onboarding/Configure/Create/Walkthrough/index.jsx
+++ components/Onboarding/Configure/Create/Walkthrough/index.jsx
@@ -7,18 +7,30 @@ import WordPrompt from './WordPrompt'
 import generateRandomWords from '../../../../../utils/generateRandomWords'
 
 const indexToWord = [
-  'first',
-  'second',
-  'third',
-  'fourth',
-  'fifth',
-  'sixth',
-  'seventh',
-  'eighth',
-  'ninth',
-  'tenth',
-  'eleventh',
-  'twelfth'
+  '1st',
+  '2nd',
+  '3rd',
+  '4th',
+  '5th',
+  '6th',
+  '7th',
+  '8th',
+  '9th',
+  '10th',
+  '11th',
+  '12th',
+  '13th',
+  '14th',
+  '15th',
+  '16th',
+  '17th',
+  '18th',
+  '19th',
+  '20th',
+  '21st',
+  '22nd',
+  '23rd',
+  '24th'
 ]
 
 const Stage = ({ mnemonic, setCanContinue, walkthroughStep }) => {
