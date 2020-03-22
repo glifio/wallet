133ccb311a18f425f79d06ce7b72821763d34e5b
Jon <Schwartz10@users.noreply.github.com>
2020-03-22 14:42:13 -0400

    Clean up scripts, change host (#241)

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,6 @@ echo "$DOCKER_PWORD" | docker login -u "$DOCKER_USER" --password-stdin
 docker push $ORG/$IMAGE
 
 # Script to stop old container and run new one with new image on Docker Hub
-SCRIPT="cd filecoin-web-wallet; git pull origin primary; VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]'); docker pull openworklabs/filecoin-web-wallet:"$VERSION"; docker stop filecoin-web-wallet; docker rm filecoin-web-wallet; docker run -d --name filecoin-web-wallet -p 3000:3000 openworklabs/filecoin-web-wallet:"$VERSION"; exit"
+SCRIPT="cd filecoin-web-wallet; git pull origin primary; make VERSION="$VERSION"; exit"
 
-ssh -i $HOME/deploy_key root@174.138.44.58 "${SCRIPT}"
+ssh -i $HOME/deploy_key root@$SERVER_IP_ADDRESS "${SCRIPT}"
