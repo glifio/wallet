bbc2117267d5dd40357777d1ba7475e5cecc15a6
Alex Singh <alex@alexsingh.com>
2020-03-23 12:54:53 -0300

    Export MnemonicWordContainer

--- components/Shared/MnemonicWord/index.js
+++ components/Shared/MnemonicWord/index.js
@@ -1,2 +1,3 @@
 export { default as DisplayWord } from './DisplayWord'
 export { default as InputWord } from './InputWord'
+export { default as MnemonicWordContainer } from './MnemonicWordContainer'
