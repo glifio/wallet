679424a3aebc1b73d6fdef6baefccb7789f6449e
Alex Singh <alex@alexsingh.com>
2020-03-24 12:35:35 -0300

    Replace StepCard with StepHeader entirely

---
+++ components/Shared/StepHeader/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Menu, MenuItem } from '../Menu'
+import Stepper from '../Stepper'
+import Loading from '../LoaderGlyph'
+import Glyph from '../Glyph'
+import ErrorGlyph from '../Glyph/ErrorGlyph'
+import { Text } from '../Typography'
+
+const StepHeader = ({
+  currentStep,
+  title,
+  glyphAcronym,
+  Icon,
+  loading,
+  totalSteps,
+  error
+}) => {
+  return (
+    <Menu
+      display='flex'
+      alignItems='center'
+      justifyContent='space-between'
+      borderColor='silver'
+      width='100%'
+    >
+      <MenuItem display='flex' justifyContent='space-between'>
+        {error && <ErrorGlyph />}
+        {loading && !error && <Loading />}
+        {!loading && !error && <Glyph Icon={Icon} acronym={glyphAcronym} />}
+      </MenuItem>
+      <MenuItem mt={3}>
+        <Text>{title}</Text>
+      </MenuItem>
+      <MenuItem>
+        <Stepper
+          textColor='text'
+          completedDotColor='status.success.background'
+          incompletedDotColor='status.inactive'
+          step={currentStep}
+          totalSteps={totalSteps}
+          ml={4}
+          my={0}
+        />
+      </MenuItem>
+    </Menu>
+  )
+}
+
+StepHeader.propTypes = {
+  title: PropTypes.string,
+  loading: PropTypes.bool,
+  currentStep: PropTypes.number.isRequired,
+  glyphAcronym: PropTypes.string,
+  Icon: PropTypes.object,
+  totalSteps: PropTypes.number.isRequired,
+  error: PropTypes.bool
+}
+
+StepHeader.defaultProps = {
+  title: '',
+  loading: false,
+  error: false
+}
+
+export default StepHeader
