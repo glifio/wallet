894b1b989664c916e13ec73a8f62b93315070b6f
Alex Singh <alex@alexsingh.com>
2020-03-30 15:12:24 -0300

    Update copy to improve clarity

--- pages/error/unsupported-browser.jsx
+++ pages/error/unsupported-browser.jsx
@@ -4,8 +4,8 @@ import { ErrorView } from '../../components/Shared'
 export default () => {
   return (
     <ErrorView
-      title='Glif only supports Chrome'
-      description='Please install Google Chrome to continue.'
+      title='Ledger only supports Chrome'
+      description='Please install Google Chrome to continue using your Ledger device, or choose an alternative setup option'
       linkDisplay='Install Google Chrome.'
       linkhref='https://www.google.com/chrome'
     />
