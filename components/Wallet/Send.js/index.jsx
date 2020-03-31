d9d93db89ef9ec3e5920f64db76cb5d48a70cbc4
Alex Singh <alex@alexsingh.com>
2020-03-31 10:38:46 -0300

    Remove border radius. Add conditional text to secondary button.

--- components/Wallet/Send.js/index.jsx
+++ components/Wallet/Send.js/index.jsx
@@ -346,7 +346,7 @@ const Send = ({ close }) => {
               <>
                 <Button
                   type='button'
-                  title='Cancel'
+                  title={step === 1 ? 'Cancel' : 'Back'}
                   variant='secondary'
                   border={0}
                   borderRight={1}
@@ -358,6 +358,10 @@ const Send = ({ close }) => {
                     resetLedgerState()
                     close()
                   }}
+                  css={`
+                    /* 'css' operation is used here to override its inherited border-radius property */
+                    border-radius: 0px;
+                  `}
                 />
                 <Button
                   border={0}
@@ -378,6 +382,10 @@ const Send = ({ close }) => {
                   title={step === 1 ? 'Next' : 'Confirm'}
                   variant='primary'
                   onClick={() => {}}
+                  css={`
+                    /* 'css' operation is used here to override its inherited border-radius property */
+                    border-radius: 0px;
+                  `}
                 />
               </>
             )}
