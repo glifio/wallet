91d7bbd7544f15886b312bfd64a0a226de266ba9
Alex Singh <alex@alexsingh.com>
2020-04-01 14:26:53 -0300

    Fix sign up display event

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Router from 'next/router'
 import styled from 'styled-components'
 import { space, layout, typography, border, color } from 'styled-system'
@@ -49,7 +49,7 @@ ${space}
   ${color}
 `
 
-const ShowSignUp = setClicked => {
+const ShowSignUp = () => {
   return (
     <>
       <InputEmail
@@ -78,270 +78,266 @@ const ShowSignUp = setClicked => {
   )
 }
 
-export default class Home extends React.Component {
-  componentDidMount() {
-    // Router.push(`/onboard?network=t`)
-  }
+export default () => {
+  const [clicked, setClicked] = useState(false)
 
-  render() {
-    return (
-      <Box
-        display='block'
-        margin='0 auto'
-        py={[2, 4]}
-        pl={[3, 4, 6, 7]}
-        pr={[3, 4, 6]}
-        maxWidth='1680px'
-        width='100%'
-        minHeight='100vh'
-        // bg='core.white'
+  return (
+    <Box
+      display='block'
+      margin='0 auto'
+      py={[2, 4]}
+      pl={[3, 4, 6, 7]}
+      pr={[3, 4, 6]}
+      maxWidth='1680px'
+      width='100%'
+      minHeight='100vh'
+      // bg='core.white'
+    >
+      <Menu
+        display='flex'
+        alignItems='center'
+        justifyContent='flex-start'
+        flexWrap='wrap'
       >
-        <Menu
-          display='flex'
-          alignItems='center'
-          justifyContent='flex-start'
-          flexWrap='wrap'
-        >
-          <MenuItem>
-            <Box
-              display='inline-block'
-              py={3}
-              mr={[4, 6, 7, 8]}
-              px={2}
-              borderRadius={4}
-              css={`
-                background: linear-gradient(180deg, #ff8d3b 0%, #ffeb80 100%);
-                border-radius: 16px;
-              `}
-            >
-              <IconGlif size={[6, 7, 8]} />
-            </Box>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>is</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>an</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>interoperable</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>set</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>of</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>tools</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <Image width='200px' alt='' src='/static/imgtools.png' />
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>for</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>the</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>Filecoin</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>network.</TitleCopy>
-          </MenuItem>
-          <MenuItem
-            display='flex'
-            flexWrap='wrap'
-            alignItems='center'
-            justifyContent='space-between'
-            width='100%'
-            color='core.darkgray'
-            my={[2, 3]}
+        <MenuItem>
+          <Box
+            display='inline-block'
+            py={3}
+            mr={[4, 6, 7, 8]}
+            px={2}
+            borderRadius={4}
+            css={`
+              background: linear-gradient(180deg, #ff8d3b 0%, #ffeb80 100%);
+              border-radius: 16px;
+            `}
           >
-            <Text fontSize={[4, 5, 6]} my={2}>
-              Be the first to learn when we launch
-            </Text>
-            <ButtonSignUp
-              background='transparent'
-              color='core.nearblack'
-              fontSize={[4, 5, 6]}
-              border={1}
-              px={6}
-              py={2}
-              height='max-content'
-              borderRadius={6}
-              onClick={() => setClicked(true)}
-            >
-              Sign Up
-            </ButtonSignUp>
-            {setClicked && <ShowSignUp />}
-          </MenuItem>
-        </Menu>
-        <Menu
+            <IconGlif size={[6, 7, 8]} />
+          </Box>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>is</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>an</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>interoperable</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>set</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>of</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>tools</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <Image width='200px' alt='' src='/static/imgtools.png' />
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>for</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>the</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>Filecoin</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>network.</TitleCopy>
+        </MenuItem>
+        <MenuItem
           display='flex'
-          alignItems='center'
-          justifyContent='flex-start'
           flexWrap='wrap'
-          my={8}
-        >
-          <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
-            <Box
-              display='inline-block'
-              py={2}
-              mr={4}
-              px={3}
-              borderRadius={4}
-              css={`
-                background: linear-gradient(
-                  180deg,
-                  #1a1a1a 2.72%,
-                  rgba(128, 128, 128, 0.6) 100%
-                );
-                border-radius: 16px;
-              `}
-            >
-              <IconGlif
-                fill='#fff'
-                size={[6, 7, 8]}
-                css={`
-                  transform: rotate(-90deg);
-                `}
-              />
-            </Box>
-            <TitleCopy>Wallet</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>A</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>lightweight</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3]} height='120px'>
-            <Image width='300px' alt='' src='/static/imgwallet.png' />
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>interface</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>for</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>sending</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>and</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>receiving</TitleCopy>
-          </MenuItem>
-
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>Filecoin</TitleCopy>
-          </MenuItem>
-        </Menu>
-        <Menu
-          display='flex'
           alignItems='center'
-          justifyContent='flex-start'
-          flexWrap='wrap'
-          my={8}
+          justifyContent='space-between'
+          width='100%'
+          color='core.darkgray'
+          my={[2, 3]}
         >
-          <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
-            <Box
-              display='inline-block'
-              py={2}
-              mr={4}
-              px={3}
-              borderRadius={4}
+          <Text fontSize={[4, 5, 6]} my={2}>
+            Be the first to learn when we launch
+          </Text>
+          <ButtonSignUp
+            background='transparent'
+            color='core.nearblack'
+            fontSize={[4, 5, 6]}
+            border={1}
+            px={6}
+            py={2}
+            height='max-content'
+            borderRadius={6}
+            onClick={() => setClicked(true)}
+          >
+            Sign Up
+          </ButtonSignUp>
+          {clicked && <ShowSignUp />}
+        </MenuItem>
+      </Menu>
+      <Menu
+        display='flex'
+        alignItems='center'
+        justifyContent='flex-start'
+        flexWrap='wrap'
+        my={8}
+      >
+        <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
+          <Box
+            display='inline-block'
+            py={2}
+            mr={4}
+            px={3}
+            borderRadius={4}
+            css={`
+              background: linear-gradient(
+                180deg,
+                #1a1a1a 2.72%,
+                rgba(128, 128, 128, 0.6) 100%
+              );
+              border-radius: 16px;
+            `}
+          >
+            <IconGlif
+              fill='#fff'
+              size={[6, 7, 8]}
               css={`
-                background: linear-gradient(
-                  180deg,
-                  #1a1a1a 2.72%,
-                  rgba(128, 128, 128, 0.6) 100%
-                );
-                border-radius: 16px;
+                transform: rotate(-90deg);
               `}
-            >
-              <IconGlif
-                fill='#fff'
-                size={[6, 7, 8]}
-                css={`
-                  transform: rotate(-90deg);
-                `}
-              />
-            </Box>
-            <TitleCopy>Node</TitleCopy>
-          </MenuItem>
+            />
+          </Box>
+          <TitleCopy>Wallet</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>A</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>A</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>fully</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>lightweight</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>managed</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3]} height='120px'>
+          <Image width='300px' alt='' src='/static/imgwallet.png' />
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>Filecoin</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>interface</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>node</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>for</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>you</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>sending</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3]} height='120px'>
-            <Image width='200px' alt='' src='/static/imgnode.png' />
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>and</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>can</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>receiving</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>deploy</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>Filecoin</TitleCopy>
+        </MenuItem>
+      </Menu>
+      <Menu
+        display='flex'
+        alignItems='center'
+        justifyContent='flex-start'
+        flexWrap='wrap'
+        my={8}
+      >
+        <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
+          <Box
+            display='inline-block'
+            py={2}
+            mr={4}
+            px={3}
+            borderRadius={4}
+            css={`
+              background: linear-gradient(
+                180deg,
+                #1a1a1a 2.72%,
+                rgba(128, 128, 128, 0.6) 100%
+              );
+              border-radius: 16px;
+            `}
+          >
+            <IconGlif
+              fill='#fff'
+              size={[6, 7, 8]}
+              css={`
+                transform: rotate(-90deg);
+              `}
+            />
+          </Box>
+          <TitleCopy>Node</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>from</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>A</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>the</TitleCopy>
-          </MenuItem>
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>fully</TitleCopy>
+        </MenuItem>
 
-          <MenuItem mr='8%' my={[2, 3, 5]}>
-            <TitleCopy>command-line</TitleCopy>
-          </MenuItem>
-        </Menu>
-      </Box>
-    )
-  }
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>managed</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>Filecoin</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>node</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>you</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3]} height='120px'>
+          <Image width='200px' alt='' src='/static/imgnode.png' />
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>can</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>deploy</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>from</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>the</TitleCopy>
+        </MenuItem>
+
+        <MenuItem mr='8%' my={[2, 3, 5]}>
+          <TitleCopy>command-line</TitleCopy>
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
 }
