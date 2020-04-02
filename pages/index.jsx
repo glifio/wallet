7be63a8c2173281c5a1262bb137bc1807e047239
Alex Singh <alex@alexsingh.com>
2020-04-02 16:40:13 -0300

    Add <section> tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -138,336 +138,343 @@ export default () => {
   const [subscribed, setSubscribed] = useState(false)
 
   return (
-    <Box
-      display='block'
-      margin='0 auto'
-      py={[2, 4]}
-      pl={[3, 4, 6, 7]}
-      pr={[3, 4, 6]}
-      maxWidth='1680px'
-      width='100%'
-      minHeight='100vh'
-      // bg='core.white'
-    >
-      <Menu
-        display='flex'
-        alignItems='center'
-        justifyContent='flex-start'
-        flexWrap='wrap'
+    <section name='Introduction'>
+      <Box
+        display='block'
+        margin='0 auto'
+        py={[2, 4]}
+        pl={[3, 4, 6, 7]}
+        pr={[3, 4, 6]}
+        maxWidth='1680px'
+        width='100%'
+        minHeight='100vh'
+        // bg='core.white'
       >
-        <MenuItem>
-          <Box
-            display='inline-block'
-            py={3}
-            mr={[4, 6, 7, 8]}
-            px={2}
-            borderRadius={4}
-            css={`
-              background: linear-gradient(180deg, #ff8d3b 0%, #ffeb80 100%);
-              border-radius: 16px;
-            `}
-          >
-            <IconGlif size={[6, 7, 8]} />
-          </Box>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>is</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>an</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>interoperable</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>set</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>of</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>tools</TitleCopy>
-        </MenuItem>
-
-        <MenuItem height='100px' mr='8%' my={[2, 3, 5]}>
-          <Image
-            width='200px'
-            alt='Credit & Source: https://www.behance.net/gallery/14115935/Lapka-PEM-Achievement-Stones'
-            src='/static/imgtools.png'
-          />
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>for</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>the</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>Filecoin</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>network.</TitleCopy>
-        </MenuItem>
-        <MenuItem
+        <Menu
           display='flex'
+          alignItems='center'
+          justifyContent='flex-start'
           flexWrap='wrap'
-          alignItems='baseline'
-          justifyContent='space-between'
-          width='100%'
-          color='core.darkgray'
-          my={[2, 3]}
         >
-          <>
-            {clicked ? (
-              <>
-                <Menu display='flex' alignItems='center'>
-                  <MenuItem>
-                    <ShowSignUp
-                      setSubscribed={setSubscribed}
-                      setError={setError}
-                    />
-
-                    {error ? (
-                      <Title mt={2} color='red'>
-                        {error}
-                      </Title>
-                    ) : (
-                      <Title mt={2}>Glif don't spam! Unsub whenever.</Title>
-                    )}
-                    {subscribed && (
-                      <Title mt={2} color='status.success.background'>
-                        You're subscribed. Keep an eye out.
-                      </Title>
-                    )}
-                  </MenuItem>
-                </Menu>
-                <ButtonSignUp
-                  width={['100%', 'auto']}
-                  background='transparent'
-                  color='core.nearblack'
-                  fontSize={[4, 5, 6]}
-                  border={1}
-                  px={6}
-                  py={2}
-                  my={2}
-                  height='max-content'
-                  borderRadius={6}
-                  onClick={() => setClicked(false)}
-                >
-                  Cancel
-                </ButtonSignUp>
-              </>
-            ) : (
-              <>
-                <Text fontSize={[4, 5, 6]} my={2}>
-                  Be the first to learn when we launch
-                </Text>
-
-                <ButtonSignUp
-                  background='transparent'
-                  color='core.nearblack'
-                  fontSize={[4, 5, 6]}
-                  border={1}
-                  px={6}
-                  py={2}
-                  height='max-content'
-                  borderRadius={6}
-                  onClick={() => setClicked(true)}
-                >
-                  Sign Up
-                </ButtonSignUp>
-              </>
-            )}
-          </>
-        </MenuItem>
-      </Menu>
-      <Menu
-        display='flex'
-        alignItems='center'
-        justifyContent='flex-start'
-        flexWrap='wrap'
-        my={8}
-      >
-        <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
-          <Box
-            display='inline-block'
-            py={2}
-            mr={4}
-            px={3}
-            borderRadius={4}
-            css={`
-              background: linear-gradient(
-                180deg,
-                #1a1a1a 2.72%,
-                rgba(128, 128, 128, 0.6) 100%
-              );
-              border-radius: 16px;
-            `}
-          >
-            <IconGlif
-              fill='#fff'
-              size={[6, 7, 8]}
+          <MenuItem>
+            <Box
+              display='inline-block'
+              py={3}
+              mr={[4, 6, 7, 8]}
+              px={2}
+              borderRadius={4}
               css={`
-                transform: rotate(-90deg);
+                background: linear-gradient(180deg, #ff8d3b 0%, #ffeb80 100%);
+                border-radius: 16px;
               `}
-            />
-          </Box>
-          <TitleCopy>Wallet</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>A</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>lightweight</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3]} height='120px'>
-          <Image width='300px' alt='' src='/static/imgwallet.png' />
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>interface</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>for</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>sending</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>and</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>receiving</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>Filecoin</TitleCopy>
-        </MenuItem>
-      </Menu>
-      <Menu
-        display='flex'
-        alignItems='center'
-        justifyContent='flex-start'
-        flexWrap='wrap'
-        my={8}
-      >
-        <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
-          <Box
-            display='inline-block'
-            py={2}
-            mr={4}
-            px={3}
-            borderRadius={4}
-            css={`
-              background: linear-gradient(
-                180deg,
-                #1a1a1a 2.72%,
-                rgba(128, 128, 128, 0.6) 100%
-              );
-              border-radius: 16px;
-            `}
-          >
-            <IconGlif
-              fill='#fff'
-              size={[6, 7, 8]}
-              css={`
-                transform: rotate(-90deg);
-              `}
-            />
-          </Box>
-          <TitleCopy>Node</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>A</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>fully</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>managed</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>Filecoin</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>node</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>you</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3]} height='120px'>
-          <Image
-            width='200px'
-            alt='Credit & Source: https://www.nontemporary.com/post/187451107349/rob-nick-carter'
-            src='/static/imgnode.png'
-          />
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>can</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>deploy</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>from</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>the</TitleCopy>
-        </MenuItem>
-
-        <MenuItem mr='8%' my={[2, 3, 5]}>
-          <TitleCopy>command-line</TitleCopy>
-        </MenuItem>
-      </Menu>
-
-      <Menu>
-        <MenuItem display='flex' alignItems='center' my={[2, 3, 5]}>
-          <IconGlif size={6} />
-          <Title my={0} mx={2}>
-            is an
-            <ExtLink
-              href='https://www.openworklabs.com'
-              color='core.primary'
-              borderBottom={1}
-              borderWidth={3}
-              fontSize={4}
-              mx={1}
-              target='_blank'
             >
-              OWL
-            </ExtLink>{' '}
-            project {'\u00A9'} 2020
-          </Title>
-        </MenuItem>
-      </Menu>
+              <IconGlif size={[6, 7, 8]} />
+            </Box>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>is</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>an</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>interoperable</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>set</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>of</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>tools</TitleCopy>
+          </MenuItem>
+
+          <MenuItem height='100px' mr='8%' my={[2, 3, 5]}>
+            <Image
+              width='200px'
+              alt='Credit & Source: https://www.behance.net/gallery/14115935/Lapka-PEM-Achievement-Stones'
+              src='/static/imgtools.png'
+            />
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>for</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>the</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>Filecoin</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>network.</TitleCopy>
+          </MenuItem>
+          <MenuItem
+            display='flex'
+            flexWrap='wrap'
+            alignItems='baseline'
+            justifyContent='space-between'
+            width='100%'
+            color='core.darkgray'
+            my={[2, 3]}
+          >
+            <>
+              {clicked ? (
+                <>
+                  <Menu display='flex' alignItems='center'>
+                    <MenuItem>
+                      <ShowSignUp
+                        setSubscribed={setSubscribed}
+                        setError={setError}
+                      />
+
+                      {error ? (
+                        <Title mt={2} color='red'>
+                          {error}
+                        </Title>
+                      ) : (
+                        <Title mt={2}>Glif don't spam! Unsub whenever.</Title>
+                      )}
+                      {subscribed && (
+                        <Title mt={2} color='status.success.background'>
+                          You're subscribed. Keep an eye out.
+                        </Title>
+                      )}
+                    </MenuItem>
+                  </Menu>
+                  <ButtonSignUp
+                    width={['100%', 'auto']}
+                    background='transparent'
+                    color='core.nearblack'
+                    fontSize={[4, 5, 6]}
+                    border={1}
+                    px={6}
+                    py={2}
+                    my={2}
+                    height='max-content'
+                    borderRadius={6}
+                    onClick={() => setClicked(false)}
+                  >
+                    Cancel
+                  </ButtonSignUp>
+                </>
+              ) : (
+                <>
+                  <Text fontSize={[4, 5, 6]} my={2}>
+                    Be the first to learn when we launch
+                  </Text>
+
+                  <ButtonSignUp
+                    background='transparent'
+                    color='core.nearblack'
+                    fontSize={[4, 5, 6]}
+                    border={1}
+                    px={6}
+                    py={2}
+                    height='max-content'
+                    borderRadius={6}
+                    onClick={() => setClicked(true)}
+                  >
+                    Sign Up
+                  </ButtonSignUp>
+                </>
+              )}
+            </>
+          </MenuItem>
+        </Menu>
+      </section>
+      <section name='Glif Wallet'>
+        <Menu
+          display='flex'
+          alignItems='center'
+          justifyContent='flex-start'
+          flexWrap='wrap'
+          my={8}
+        >
+          <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
+            <Box
+              display='inline-block'
+              py={2}
+              mr={4}
+              px={3}
+              borderRadius={4}
+              css={`
+                background: linear-gradient(
+                  180deg,
+                  #1a1a1a 2.72%,
+                  rgba(128, 128, 128, 0.6) 100%
+                );
+                border-radius: 16px;
+              `}
+            >
+              <IconGlif
+                fill='#fff'
+                size={[6, 7, 8]}
+                css={`
+                  transform: rotate(-90deg);
+                `}
+              />
+            </Box>
+            <TitleCopy>Wallet</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>A</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>lightweight</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3]} height='120px'>
+            <Image width='300px' alt='' src='/static/imgwallet.png' />
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>interface</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>for</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>sending</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>and</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>receiving</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>Filecoin</TitleCopy>
+          </MenuItem>
+        </Menu>
+      </section>
+      <section name='Glif Node'>
+        <Menu
+          display='flex'
+          alignItems='center'
+          justifyContent='flex-start'
+          flexWrap='wrap'
+          my={8}
+        >
+          <MenuItem display='flex' alignItems='center' width='100%' mb={5}>
+            <Box
+              display='inline-block'
+              py={2}
+              mr={4}
+              px={3}
+              borderRadius={4}
+              css={`
+                background: linear-gradient(
+                  180deg,
+                  #1a1a1a 2.72%,
+                  rgba(128, 128, 128, 0.6) 100%
+                );
+                border-radius: 16px;
+              `}
+            >
+              <IconGlif
+                fill='#fff'
+                size={[6, 7, 8]}
+                css={`
+                  transform: rotate(-90deg);
+                `}
+              />
+            </Box>
+            <TitleCopy>Node</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>A</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>fully</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>managed</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>Filecoin</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>node</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>you</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3]} height='120px'>
+            <Image
+              width='200px'
+              alt='Credit & Source: https://www.nontemporary.com/post/187451107349/rob-nick-carter'
+              src='/static/imgnode.png'
+            />
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>can</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>deploy</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>from</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>the</TitleCopy>
+          </MenuItem>
+
+          <MenuItem mr='8%' my={[2, 3, 5]}>
+            <TitleCopy>command-line</TitleCopy>
+          </MenuItem>
+        </Menu>
+      </section>
+      <section name='Made by OWL'>
+        <Menu>
+          <MenuItem display='flex' alignItems='center' my={[2, 3, 5]}>
+            <IconGlif size={6} />
+            <Title my={0} mx={2}>
+              is an
+              <ExtLink
+                href='https://www.openworklabs.com'
+                color='core.primary'
+                borderBottom={1}
+                borderWidth={3}
+                fontSize={4}
+                mx={1}
+                target='_blank'
+              >
+                OWL
+              </ExtLink>{' '}
+              project {'\u00A9'} 2020
+            </Title>
+          </MenuItem>
+        </Menu>
+      </section>
     </Box>
   )
 }
