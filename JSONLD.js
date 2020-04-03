edab3c188366e5e62b4187073a8c5569ccefd2dc
Thomas O'Brien <tbobrien612@gmail.com>
2020-04-03 07:41:24 -0700

    Add schema.org JSON-LD (#304)

---
+++ JSONLD.js
@@ -0,0 +1,43 @@
+export default {
+  '@context': 'http://schema.org/',
+  '@type': 'Organization',
+  name: 'Glif',
+  description: 'An interoperable set of tools on the Filecoin network.',
+  url: 'https://glif.io',
+  sameAs: ['https://twitter.com/GLIFio'],
+  owns: [
+    {
+      '@type': 'Product',
+      name: 'Glif Wallet',
+      description:
+        'A web wallet to manage your Filecoin on your Ledger device.',
+      category: 'Blockchain software'
+    },
+    {
+      '@type': 'Product',
+      name: 'Glif Nodes',
+      description: 'Private and public gateways to the Filecoin network.',
+      category: 'Blockchain software'
+    }
+  ],
+  knowsAbout: [
+    {
+      '@type': 'Product',
+      name: 'Filecoin',
+      url: 'https://filecoin.io',
+      category: 'Blockchain network'
+    }
+  ],
+  parentOrganization: {
+    '@type': 'Organization',
+    name: 'Open Work Labs',
+    description:
+      'A product studio building tools for open work and the distributed web.',
+    url: 'https://openworklabs.com',
+    sameAs: [
+      'https://github.com/openworklabs',
+      'https://twitter.com/openworklabs',
+      'https://www.are.na/open-work-labs'
+    ]
+  }
+}
