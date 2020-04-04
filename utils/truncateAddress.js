d0e539e5da7da25cf98b231aeb3603572ae66bcc
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-04 16:30:35 -0400

    Fix failing test

--- utils/truncateAddress.js
+++
@@ -1,1 +0,0 @@
-export default address => `${address.slice(0, 4)} ... ${address.slice(-4)}`
--- utils/truncateAddress/index.test.js
+++ utils/truncateAddress/index.test.js
@@ -5,7 +5,7 @@ describe('truncateAddress', () => {
     const truncated = truncateAddress(
       't1mbk7q6gm4rjlndfqw6f2vkfgqotres3fgicb2uq'
     )
-    expect(truncated).toBe('t1mb...b2uq')
-    expect(truncated.split('...').join('').length).toBe(8)
+    expect(truncated).toBe('t1mbk ... b2uq')
+    expect(truncated.split('...').join('').length).toBe(11)
   })
 })
