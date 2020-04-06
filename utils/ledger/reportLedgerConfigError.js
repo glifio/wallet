906e76e5e77eaed73f4f92e7cf7149db8ff92a66
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-06 15:22:16 -0400

    Add more descriptive error messages

--- utils/ledger/reportLedgerConfigError.js
+++ utils/ledger/reportLedgerConfigError.js
@@ -10,8 +10,10 @@ export const reportLedgerConfigError = ({
   if (connectedFailure) return 'Is your Ledger device plugged in?'
   if (busy) return 'Is your Ledger device busy?'
   if (ledgerLocked) return 'Is your Ledger device unlocked?'
-  if (filecoinAppNotOpen) return 'Is the Filecoin App open on your device?'
-  if (replug) return 'Please unplug and replug your device, and try again.'
+  if (filecoinAppNotOpen)
+    return 'Is the Filecoin App open on your Ledger device?'
+  if (replug)
+    return 'Please unplug and replug your Ledger device, and try again.'
   if (inUseByAnotherApp)
     return 'Please quit any other App using your Ledger device.'
   if (otherError) return otherError && otherError.message
