5647a3259b153cdc42c76e6190aed39291b94e44
Alex Singh <alex@alexsingh.com>
2020-04-07 15:56:53 -0300

    Set transparent b/g for all disabled inputs

--- components/Shared/Input/BaseInput.js
+++ components/Shared/Input/BaseInput.js
@@ -22,6 +22,7 @@ export default styled.input.attrs(props => ({
   background: ${props => {
     if (props.valid) return props.theme.colors.input.background.valid
     if (props.error) return props.theme.colors.input.background.invalid
+    if (props.disabled) return props.theme.colors.core.transparent
     return props.theme.colors.input.background.base
   }};
 
