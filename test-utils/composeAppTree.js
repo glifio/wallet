ff9fd090f2abbf7f578af885c089f94e8d9b1131
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-07 18:12:09 -0400

    Initial commit wip

---
+++ test-utils/composeAppTree.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FilecoinNumber } from '@openworklabs/filecoin-number'
+import { Provider } from 'react-redux'
+
+import { initializeStore } from '../test-utils/index'
+import { initialState } from '../store/states'
+
+const defaultPreset = {}
+
+const composeAppTree = (preset = defaultPreset, options = {}) => {
+  const state = {}
+}
