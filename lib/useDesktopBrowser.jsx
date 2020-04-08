54600e9d419146bae758e889f52b9a1ad642da74
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-08 14:19:30 -0400

    Renaming

--- lib/useDesktopBrowser.jsx
+++ lib/useDesktopBrowser.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-import isMobileBrowser from '../utils/isMobileBrowser'
+import isMobileOrTableBrowser from '../utils/isMobileOrTablet'
 
 export default () => {
   const { replace } = useRouter()
   useEffect(() => {
-    const onMobileBrowser = isMobileBrowser()
+    const onMobileBrowser = isMobileOrTableBrowser()
     if (onMobileBrowser) replace('/error/use-desktop-browser')
   }, [replace])
 }
