6bc4b21f213d75c69f71c4c9a0c75ca7a4699ebe
Alex Singh <alex@alexsingh.com>
2020-04-08 17:39:11 -0300

    Update Warning Card content

--- components/Shared/Warning/index.jsx
+++ components/Shared/Warning/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import WarningCard from './WarningCard'
 
-export const WarningScreen = () => {
+export default () => {
   return (
     <WarningCard
-      title='Ledger only supports Chrome'
-      description='Please install Google Chrome to continue using your Ledger device, or choose an alternative setup option'
-      linkDisplay='Install Google Chrome.'
+      title='Warning'
+      description='We do not recommend you use any browser-based wallet to transact large sums of Filecoin. Glif should only be used with a Ledger hardware wallet.'
+      linkDisplay="Why isn't it secure?"
       linkhref='https://www.google.com/chrome'
     />
   )
