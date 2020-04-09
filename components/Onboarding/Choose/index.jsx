ff166831a4149b8d1b7049fe87e16da8b4278909
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-09 16:49:14 -0400

    Stub link for more info

--- components/Onboarding/Choose/index.jsx
+++ components/Onboarding/Choose/index.jsx
@@ -38,7 +38,7 @@ export default () => {
           title='Warning'
           description='We do not recommend you use any browser-based wallet to transact large sums of Filecoin. Glif should only be used with a Ledger hardware wallet.'
           linkDisplay="Why isn't it secure?"
-          linkhref='https://www.google.com/chrome'
+          linkhref='https://coinsutra.com/security-risks-bitcoin-wallets/'
           onBack={() => setLocalWalletType(null)}
           onAccept={onChoose}
         />
