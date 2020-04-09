083a074157572d7d316a0b082e63151859748797
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-09 16:47:05 -0400

    Add warning for non-ledger wallet strategies

--- components/Onboarding/Choose/index.jsx
+++ components/Onboarding/Choose/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Box, IconLedger } from '../../Shared'
+import React, { useState } from 'react'
+import { Box, IconLedger, Warning } from '../../Shared'
 import ImportWallet from './Import'
 import CreateWallet from './Create'
 import Hello from './Hello'
@@ -13,49 +13,85 @@ import { useWalletProvider } from '../../../WalletProvider'
 
 export default () => {
   const { setWalletType } = useWalletProvider()
-  return (
-    <Box
-      display='flex'
-      justifyContent='center'
-      flexDirection='column'
-      alignItems='center'
-      minHeight='90vh'
-    >
-      <Hello textAlign='center' mb={4} />
+  // this could be cleaner, but we use this to more easily navigate to/from the warning card
+  const [localWalletType, setLocalWalletType] = useState(null)
 
-      <Box display='flex' justifyContent='center' flexDirection='column' mr={3}>
-        <Box display='flex' flexWrap='wrap' justifyContent='center' mb={5}>
-          <ImportWallet
-            onClick={() => setWalletType(LEDGER)}
-            Icon={IconLedger}
-            title='Ledger Device'
-            description='Use your Ledger device'
-            tag='Most Secure'
-            m={2}
-            height='240px'
+  const onChoose = type => {
+    if (
+      !localWalletType &&
+      (type === CREATE_MNEMONIC ||
+        type === IMPORT_MNEMONIC ||
+        type === IMPORT_SINGLE_KEY)
+    ) {
+      setLocalWalletType(type)
+    } else if (localWalletType) {
+      setWalletType(localWalletType)
+    } else {
+      setWalletType(type)
+    }
+  }
+
+  return (
+    <>
+      {localWalletType ? (
+        <Warning
+          title='Warning'
+          description='We do not recommend you use any browser-based wallet to transact large sums of Filecoin. Glif should only be used with a Ledger hardware wallet.'
+          linkDisplay="Why isn't it secure?"
+          linkhref='https://www.google.com/chrome'
+          onBack={() => setLocalWalletType(null)}
+          onAccept={onChoose}
+        />
+      ) : (
+        <Box
+          display='flex'
+          justifyContent='center'
+          flexDirection='column'
+          alignItems='center'
+          minHeight='90vh'
+        >
+          <Hello textAlign='center' mb={4} />
+
+          <Box
             display='flex'
-            justifyContent='space-between'
+            justifyContent='center'
             flexDirection='column'
-          />
-          <CreateWallet onClick={() => setWalletType(CREATE_MNEMONIC)} m={2} />
+            mr={3}
+          >
+            <Box display='flex' flexWrap='wrap' justifyContent='center' mb={5}>
+              <ImportWallet
+                onClick={() => onChoose(LEDGER)}
+                Icon={IconLedger}
+                title='Ledger Device'
+                description='Use your Ledger device'
+                tag='Most Secure'
+                m={2}
+                height='240px'
+                display='flex'
+                justifyContent='space-between'
+                flexDirection='column'
+              />
+              <CreateWallet onClick={() => onChoose(CREATE_MNEMONIC)} m={2} />
+            </Box>
+            <Box display='flex' flexWrap='wrap' justifyContent='center'>
+              <ImportWallet
+                onClick={() => onChoose(IMPORT_MNEMONIC)}
+                glyphAcronym='Sp'
+                title='Import Seed Phrase'
+                description='Use your existing seed phrase'
+                m={2}
+              />
+              <ImportWallet
+                onClick={() => onChoose(IMPORT_SINGLE_KEY)}
+                glyphAcronym='Pk'
+                title='Import Private Key'
+                description='Use your existing private key'
+                m={2}
+              />
+            </Box>
+          </Box>
         </Box>
-        <Box display='flex' flexWrap='wrap' justifyContent='center'>
-          <ImportWallet
-            onClick={() => setWalletType(IMPORT_MNEMONIC)}
-            glyphAcronym='Sp'
-            title='Import Seed Phrase'
-            description='Use your existing seed phrase'
-            m={2}
-          />
-          <ImportWallet
-            onClick={() => setWalletType(IMPORT_SINGLE_KEY)}
-            glyphAcronym='Pk'
-            title='Import Private Key'
-            description='Use your existing private key'
-            m={2}
-          />
-        </Box>
-      </Box>
-    </Box>
+      )}
+    </>
   )
 }
