207b6baf56b7893dd191baded845fd62904d9305
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-13 16:56:43 -0400

    Fix failing tests

--- components/Wallet/Send.js/index.test.jsx
+++ components/Wallet/Send.js/index.test.jsx
@@ -10,12 +10,16 @@ jest.mock('@openworklabs/filecoin-wallet-provider')
 describe('Send Flow', () => {
   let close = () => {}
   beforeEach(() => {
+    jest.useFakeTimers()
     jest.clearAllMocks()
     close = jest.fn()
   })
 
   describe('Sending a message', () => {
-    afterEach(cleanup)
+    afterEach(() => {
+      jest.clearAllTimers()
+      cleanup()
+    })
 
     test('it allows a user to send a message', async () => {
       const { Tree, store, walletProvider } = composeMockAppTree('postOnboard')
@@ -43,10 +47,11 @@ describe('Send Flow', () => {
       })
       expect(walletProvider.getNonce).toHaveBeenCalledWith(address)
       expect(walletProvider.wallet.sign).toHaveBeenCalled()
-      expect(walletProvider.wallet.sign.mock.calls[0][1].GasPrice).toBe('1')
-      expect(walletProvider.wallet.sign.mock.calls[0][1].GasLimit).toBe('1000')
-      expect(walletProvider.wallet.sign.mock.calls[0][1].Value).toBe('0.01')
-      expect(walletProvider.wallet.sign.mock.calls[0][1].To).toBe(address)
+      const message = walletProvider.wallet.sign.mock.calls[0][1].encode()
+      expect(message.GasPrice).toBe('1')
+      expect(message.GasLimit).toBe('1000')
+      expect(message.Value).toBe('10000000000000000')
+      expect(message.To).toBe(address)
 
       expect(store.getState().messages.pending.length).toBe(1)
     })
@@ -212,7 +217,8 @@ describe('Send Flow', () => {
       await flushPromises()
 
       expect(walletProvider.getNonce).toHaveBeenCalled()
-      expect(walletProvider.wallet.sign.mock.calls[0][1].GasPrice).toBe('2')
+      const message = walletProvider.wallet.sign.mock.calls[0][1].encode()
+      expect(message.GasPrice).toBe('2')
       expect(store.getState().messages.pending.length).toBe(1)
     })
 
@@ -257,7 +263,8 @@ describe('Send Flow', () => {
         await flushPromises()
       })
       expect(walletProvider.getNonce).toHaveBeenCalled()
-      expect(walletProvider.wallet.sign.mock.calls[0][1].GasLimit).toBe('2000')
+      const message = walletProvider.wallet.sign.mock.calls[0][1].encode()
+      expect(message.GasLimit).toBe('2000')
       expect(store.getState().messages.pending.length).toBe(1)
     })
 
@@ -279,9 +286,38 @@ describe('Send Flow', () => {
             value: 2
           }
         })
+        jest.runOnlyPendingTimers()
+      })
+      await flushPromises()
+      expect(walletProvider.estimateGas).toHaveBeenCalledTimes(2)
+    })
+
+    test('it sends the user to the message history after message successfully sent', async () => {
+      const { Tree } = composeMockAppTree('postOnboard')
+      const address = 't1z225tguggx4onbauimqvxzutopzdr2m4s6z6wgi'
+      const filAmount = new FilecoinNumber('.01', 'fil')
+      await act(async () => {
+        render(
+          <Tree>
+            <Send close={close} />
+          </Tree>
+        )
+        fireEvent.change(screen.getByPlaceholderText(/t1.../), {
+          target: { value: address }
+        })
+        fireEvent.change(screen.getAllByPlaceholderText('0')[0], {
+          target: { value: filAmount }
+        })
+        await flushPromises()
+        fireEvent.blur(screen.getAllByPlaceholderText('0')[0])
+        await flushPromises()
+        fireEvent.click(screen.getByText('Send'))
+        await flushPromises()
+        fireEvent.click(screen.getByText('Confirm'))
         await flushPromises()
       })
-      expect(walletProvider.estimateGas).toHaveBeenCalledTimes(2)
+      await flushPromises()
+      expect()
     })
   })
 })
