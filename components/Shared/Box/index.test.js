ba3cd78fc545bca9dfdceb3b993a3590e2969c64
listenaddress <tbobrien612@gmail.com>
2020-04-13 11:02:51 -0700

    Add box tests

---
+++ components/Shared/Box/index.test.js
@@ -0,0 +1,36 @@
+import { cleanup, render, screen, act, fireEvent } from '@testing-library/react'
+import Box from '.'
+import composeMockAppTree from '../../../test-utils/composeMockAppTree'
+
+describe('Box', () => {
+  afterEach(cleanup)
+  test('renders the box', () => {
+    const { container } = render(<Box />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('renders the box with the right atributes', () => {
+    const { Tree } = composeMockAppTree('postOnboard')
+    const { container, getByText } = render(
+      <Box
+        position='flex'
+        flexDirection='row'
+        border='1'
+        width='auto'
+        alignItems='center'
+        justifyContent='space-between'
+        bg='blue'
+        p={4}
+      />
+    )
+
+    const box = container.querySelector('div')
+    expect(box).toHaveStyle('width: auto;')
+    expect(box).toHaveStyle('position: flex;')
+    expect(box).toHaveStyle('border: 1;')
+    expect(box).toHaveStyle('align-items: center;')
+    expect(box).toHaveStyle('justify-content: space-between;')
+    expect(box).toHaveStyle('background-color: blue;')
+    expect(box).toHaveStyle('padding: 32px;')
+  })
+})
