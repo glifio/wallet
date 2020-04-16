cfdd2367c3ffaaaa9ec406058669ce3d234468b6
Alex Singh <alex@alexsingh.com>
2020-04-16 14:35:01 -0300

    Update StyledALink styling & hover state

--- components/Shared/Link/index.jsx
+++ components/Shared/Link/index.jsx
@@ -8,6 +8,12 @@ export const StyledATag = styled.a.attrs(props => ({
   fontSize: 3,
   ...props
 }))`
+  text-decoration: none;
+  transition: 0.18s ease-in-out;
+  border-bottom: 2px solid ${props => props.theme.colors.core.primary}00;
+  &:hover {
+    border-bottom: 2px solid ${props => props.theme.colors.core.primary};
+  }
   ${color}
   ${typography}
   ${layout}
