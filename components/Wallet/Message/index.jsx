1549951072c293ea2d4b561abb80fa7dccd36b62
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-16 14:30:05 -0400

    Use right hook

--- components/Wallet/Message/index.jsx
+++ components/Wallet/Message/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import MessageDetail from './Detail'
 import { MessageHistoryTable } from '../../Shared'
 import useWallet from '../../../WalletProvider/useWallet'
-import useTransactionHistory from './useFilscanTransactionHistory'
+import useTransactionHistory from './useFilscoutTransactionHistory'
 
 export default () => {
   const [selectedMessageCid, setSelectedMessageCid] = useState('')
