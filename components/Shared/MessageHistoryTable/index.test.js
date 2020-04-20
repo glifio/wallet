0d06af18446e9662037b7f05e5e538845a0bd946
listenaddress <tbobrien612@gmail.com>
2020-04-19 20:29:23 -0700

    Add more tests

--- components/Shared/MessageHistoryTable/index.test.js
+++ components/Shared/MessageHistoryTable/index.test.js
@@ -33,5 +33,122 @@ describe('MessageHistoryTable', () => {
       </Tree>
     )
     expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByText('Transaction History')).toBeInTheDocument()
+  })
+
+  test('renders the loading screen when loading is true', () => {
+    const { Tree } = composeMockAppTree('postOnboard')
+    render(
+      <Tree>
+        <MessageHistoryTable
+          address='t01'
+          messages={formatFilscoutMessages(filscoutMockData).map(msg => ({
+            ...msg,
+            status: 'confirmed'
+          }))}
+          loading={true}
+          selectMessage={setSelectedMessageCid}
+          paginating={false}
+          showMore={showMore}
+          total={16}
+        />
+      </Tree>
+    )
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument()
+  })
+
+  test('renders the empty history component when messages are empty', () => {
+    const { Tree } = composeMockAppTree('postOnboard')
+    render(
+      <Tree>
+        <MessageHistoryTable
+          address='t01'
+          messages={[]}
+          loading={false}
+          selectMessage={setSelectedMessageCid}
+          paginating={false}
+          showMore={showMore}
+          total={0}
+        />
+      </Tree>
+    )
+
+    expect(
+      screen.getByText('How do I see my transaction history?')
+    ).toBeInTheDocument()
+  })
+
+  test('renders the show more button when the total is larger than # of messages in the table', () => {
+    const { Tree } = composeMockAppTree('postOnboard')
+    render(
+      <Tree>
+        <MessageHistoryTable
+          address='t01'
+          messages={formatFilscoutMessages(filscoutMockData).map(msg => ({
+            ...msg,
+            status: 'confirmed'
+          }))}
+          loading={false}
+          selectMessage={setSelectedMessageCid}
+          paginating={false}
+          showMore={showMore}
+          total={16}
+        />
+      </Tree>
+    )
+
+    expect(screen.getByText('View More')).toBeInTheDocument()
+  })
+
+  test("doesn't render the show more button when the total is larger than # of messages in the table", () => {
+    const { Tree } = composeMockAppTree('postOnboard')
+    render(
+      <Tree>
+        <MessageHistoryTable
+          address='t01'
+          messages={formatFilscoutMessages(filscoutMockData).map(msg => ({
+            ...msg,
+            status: 'confirmed'
+          }))}
+          loading={false}
+          selectMessage={setSelectedMessageCid}
+          paginating={false}
+          showMore={showMore}
+          total={filscoutMockData.length}
+        />
+      </Tree>
+    )
+
+    let error
+    try {
+      screen.getByText('View More')
+    } catch (e) {
+      error = e
+    }
+
+    expect(error).toBeTruthy()
+  })
+
+  test('renders value of first message', () => {
+    const { Tree } = composeMockAppTree('postOnboard')
+    render(
+      <Tree>
+        <MessageHistoryTable
+          address='t01'
+          messages={formatFilscoutMessages(filscoutMockData).map(msg => ({
+            ...msg,
+            status: 'confirmed'
+          }))}
+          loading={false}
+          selectMessage={setSelectedMessageCid}
+          paginating={false}
+          showMore={showMore}
+          total={filscoutMockData.length}
+        />
+      </Tree>
+    )
+
+    expect(screen.getAllByText(filscoutMockData[0].value)).toBeTruthy()
   })
 })
