134710c5a3f8a43ae94b5682cbe55945e1c14b78
Jon <Schwartz10@users.noreply.github.com>
2020-05-18 17:20:16 -0400

    Remove webHID transport and upgrade webUSB (#408)

--- utils/ledger/createTransport.js
+++ utils/ledger/createTransport.js
@@ -1,27 +1,3 @@
-import TransportWebHID from '@ledgerhq/hw-transport-webhid'
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb'
 
-export default () =>
-  new Promise((resolve, reject) => {
-    TransportWebHID.create()
-      .then(resolve)
-      .catch(err => {
-        if (
-          err.message.toLowerCase().includes('navigator.hid is not supported')
-        ) {
-          return TransportWebUSB.create()
-        }
-        if (
-          err.message.toLowerCase().includes('transporterror: invalid channel')
-        ) {
-          reject(
-            new Error(
-              'Lost connection with Ledger. Please unplug and replug device.'
-            )
-          )
-        }
-        reject(err)
-      })
-      .then(resolve)
-      .catch(reject)
-  })
+export default () => TransportWebUSB.create()
