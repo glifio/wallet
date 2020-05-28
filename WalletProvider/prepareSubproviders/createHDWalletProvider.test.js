6b8a0fcf180737d5f50f017575b62190242dcb19
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-05-28 17:58:03 -0400

    Add tests

---
+++ WalletProvider/prepareSubproviders/createHDWalletProvider.test.js
@@ -0,0 +1,57 @@
+import Message from '@openworklabs/filecoin-message'
+import createHDWalletProvider from './createHDWalletProvider'
+import mockRustModule from '@zondax/filecoin-signer-wasm'
+import { HD_WALLET } from '../../constants'
+
+jest.mock('@zondax/filecoin-signer-wasm')
+
+const mnemonic =
+  'slender spread awkward chicken noise useful thank dentist tip bronze ritual explain version spot collect whisper glow peanut bus local country album punch frown'
+
+const followsSubproviderInterface = subprovider =>
+  !!(subprovider.sign && subprovider.getAccounts && subprovider.type)
+
+describe('createHDWalletProvider', () => {
+  beforeEach(jest.clearAllMocks)
+
+  test('it returns an HDWallet subprovider that follows the wallet subprovider interface', () => {
+    const HDWalletProvider = createHDWalletProvider(mockRustModule)
+    const hdWalletProvider = new HDWalletProvider(mnemonic)
+    expect(followsSubproviderInterface(hdWalletProvider)).toBe(true)
+    expect(hdWalletProvider.type).toBe(HD_WALLET)
+  })
+
+  describe('HDWalletProvider', () => {
+    describe('sign', () => {
+      beforeEach(jest.clearAllMocks)
+
+      test('it calls the transactionSign wasm method', async () => {
+        const msg = new Message({
+          from: 't1hvuzpfdycc6z6mjgbiyaiojikd6wk2vwy7muuei',
+          to: 't1t5gdjfb6jojpivbl5uek6vf6svlct7dph5q2jwa',
+          value: '1000',
+          method: 0,
+          gasPrice: '1',
+          gasLimit: 1000,
+          nonce: 0
+        })
+        const HDWalletProvider = createHDWalletProvider(mockRustModule)
+        const hdWalletProvider = new HDWalletProvider(mnemonic)
+        await hdWalletProvider.sign("m/44'/461'/0/0/2", msg)
+        expect(mockRustModule.transactionSign).toHaveBeenCalled()
+      })
+    })
+
+    describe('getAccounts', () => {
+      beforeEach(jest.clearAllMocks)
+
+      test('it returns an array of accounts', async () => {
+        const HDWalletProvider = createHDWalletProvider(mockRustModule)
+        const hdWalletProvider = new HDWalletProvider(mnemonic)
+        const accounts = await hdWalletProvider.getAccounts(0, 5, 't')
+        expect(accounts.length).toBe(5)
+        expect(mockRustModule.keyDerive).toHaveBeenCalledTimes(5)
+      })
+    })
+  })
+})
