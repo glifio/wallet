9f791fcc3bd4e731145fd5411fc77202efcf594c
Jon <Schwartz10@users.noreply.github.com>
2020-06-19 11:04:24 -0400

    Change next.config to point back to proxy (#421)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   env: {
-    LOTUS_NODE_JSONRPC:
-      'https://node.glif.io/8b2f2b24-ae5d-43fe-b475-d4cd1a7a4dbd/rpc/v0',
+    LOTUS_NODE_JSONRPC: 'https://proxy.openworklabs.com/rpc/v0',
     WALLET_HOME: 'home'
   }
 }
