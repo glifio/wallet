1aab9ebf9e540c5a7d41795d1a806cd0797b2d4c
Jon <Schwartz10@users.noreply.github.com>
2020-07-12 15:14:07 -0400

    Use closures to protect private information (#444)

--- WalletProvider/prepareSubproviders/createSingleKeyProvider.js
+++ WalletProvider/prepareSubproviders/createSingleKeyProvider.js
@@ -1,18 +1,24 @@
 import { SINGLE_KEY } from '../../constants'
 
 export default rustModule => {
+  // this is a v weird pattern
+  // but we close over this private variable to protect it from being accessed from the outside world
+  // in the future we could transform this whole closure system into functions and away from a mix of
+  // closures && classes, but for now we're still using the `new` keyword (bc of the ledger provider)
+  // so this is the fastest approach
+  let privPrivateKey = ''
   return class SingleKeyProvider {
     constructor(privateKey) {
-      this.privateKey = privateKey
+      privPrivateKey = privateKey
       this.type = SINGLE_KEY
     }
 
     getAccounts = async (_, __, network = 't') => {
-      return [rustModule.keyRecover(this.privateKey, network === 't').address]
+      return [rustModule.keyRecover(privPrivateKey, network === 't').address]
     }
 
     sign = async (_, filecoinMessage) => {
-      const { private_hexstring } = rustModule.keyRecover(this.privateKey)
+      const { private_hexstring } = rustModule.keyRecover(privPrivateKey)
       const { signature } = rustModule.transactionSign(
         filecoinMessage.toString(),
         private_hexstring
