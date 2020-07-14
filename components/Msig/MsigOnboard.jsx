c7f8848f772eb015c2aa551f0d1c9bc2bb47de80
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-07-14 09:46:18 -0400

    Fix ledger error handling

--- components/Msig/MsigOnboard.jsx
+++ components/Msig/MsigOnboard.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Ledger from '../Onboarding/Configure/Ledger'
 import { Box } from '../Shared'
+import { useWalletProvider } from '../../WalletProvider'
+import { LEDGER } from '../../constants'
 
 export default () => {
+  const [mounted, setMounted] = useState(false)
+  const { setWalletType } = useWalletProvider()
+
+  useEffect(() => {
+    if (!mounted) {
+      setMounted(true)
+      setWalletType(LEDGER)
+    }
+  }, [mounted, setMounted, setWalletType])
   return (
     <Box
       display='flex'
