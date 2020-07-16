311d6f3799a4dfc27ad96a58fd401b3942a4ff5f
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-07-16 14:43:12 -0400

    Add tests for createLedgerProvider

---
+++ test-utils/mocks/mock-ledger-filecoin.js
@@ -0,0 +1,40 @@
+const errorFreeReturn = {
+  error_message: 'no errors'
+}
+
+export const mockSign = jest.fn().mockImplementation(async (path, msg) => {
+  return Promise.resolve({
+    ...errorFreeReturn,
+    signature_compact: new Buffer('fake signature')
+  })
+})
+
+export const mockGetAddressAndPubKey = jest
+  .fn()
+  .mockImplementation(async path => {
+    // here we just randomize addresses based on the last index in the path
+    // just for returning different addresses
+    return Promise.resolve({
+      ...errorFreeReturn,
+      addrString: `t137sjdbgunloi7couiy${
+        path.split('/')[5]
+      }l5nc7pd6k2jmq32vizpy`
+    })
+  })
+
+export const mockGetVersion = jest
+  .fn()
+  .mockImplementation(
+    () =>
+      new Promise(resolve => setTimeout(() => resolve(errorFreeReturn), 500))
+  )
+
+class Filecoin {
+  sign = (msg, path) => mockSign(path, msg)
+
+  getAddressAndPubKey = path => mockGetAddressAndPubKey(path)
+
+  getVersion = () => mockGetVersion()
+}
+
+export default Filecoin
