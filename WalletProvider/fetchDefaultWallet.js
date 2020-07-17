92d85bb5e27c7346fb25c77c8503fd6c2a1c6747
Jon <Schwartz10@users.noreply.github.com>
2020-07-17 09:55:46 -0400

    Allow errors to propagate from fetchDefaultWallet (#455)

--- WalletProvider/fetchDefaultWallet.js
+++ WalletProvider/fetchDefaultWallet.js
@@ -5,7 +5,6 @@ import {
 } from '../utils/ledger/setLedgerProvider'
 import { clearError, resetLedgerState } from './state'
 import createPath from '../utils/createPath'
-import reportError from '../utils/reportError'
 
 // a helper function for getting the default wallet associated with the wallet provider
 const fetchDefaultWallet = async (
@@ -29,20 +28,17 @@ const fetchDefaultWallet = async (
     const configured = await checkLedgerConfiguration(dispatch, provider)
     if (!configured) return null
   }
-  try {
-    const [defaultAddress] = await provider.wallet.getAccounts(network, 0, 1)
-    const balance = await provider.getBalance(defaultAddress)
-    const networkCode = network === 'f' ? 461 : 1
 
-    let path = createPath(networkCode, 0)
-    if (provider.wallet.type === SINGLE_KEY) path = SINGLE_KEY
-    return {
-      balance,
-      address: defaultAddress,
-      path
-    }
-  } catch (err) {
-    reportError(7, true, err.message, err.stack)
+  const [defaultAddress] = await provider.wallet.getAccounts(network, 0, 1)
+  const balance = await provider.getBalance(defaultAddress)
+  const networkCode = network === 'f' ? 461 : 1
+
+  let path = createPath(networkCode, 0)
+  if (provider.wallet.type === SINGLE_KEY) path = SINGLE_KEY
+  return {
+    balance,
+    address: defaultAddress,
+    path
   }
 }
 
