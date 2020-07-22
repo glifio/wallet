31af701c34801e7bc690edec5baa0d5f256a3e8e
Alex Singh <alex@alexsingh.com>
2020-07-22 11:53:19 -0300

    Update structure, colors

--- components/Shared/NodeConnected/index.jsx
+++ components/Shared/NodeConnected/index.jsx
@@ -1,26 +1,41 @@
 import { useEffect, useRef, useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import calcConnectionStrength from './connectionStrength'
 import noop from '../../../utils/noop'
 import reportError from '../../../utils/reportError'
 import Box from '../Box'
 import { Text } from '../Typography'
 
-const dotColor = connectionStrength => {
-  if (connectionStrength === -1) return 'Yellow'
-  if (connectionStrength === 0) return 'Red'
-  if (connectionStrength === 1) return 'Orange'
-  if (connectionStrength === 2) return 'Green'
+const dotColor = ({ connectionStrength, theme: { colors } }) => {
+  if (connectionStrength === -1) return colors.status.pending.background
+  if (connectionStrength === 0) return colors.status.fail.background
+  if (connectionStrength === 1) return colors.status.fail.background
+  if (connectionStrength === 2) return colors.status.success.background
 }
+
+const ColoredDotAnimator = keyframes`
+0% {
+  transform: translateX(0px)
+}
+
+50% { 
+  transform: translateX(8px)
+}
+
+100% { 
+  transform: translate(0px)
+}
+`
 
 const ColoredDot = styled.span`
   height: 8px;
   width: 8px;
-  background-color: ${props => dotColor(props.connectionStrength)};
+  background-color: ${dotColor};
   border-radius: 50%;
   display: inline-block;
   margin: 4px;
+  animation: ColoredDotAnimator 0.24s linear;
 `
 
 const NodeConnectedWidget = ({
@@ -59,17 +74,30 @@ const NodeConnectedWidget = ({
   }, [pollConnection])
 
   const nodeConnectedText = () => {
-    if (connectionStrength === -1) return 'Connecting to node...'
+    // Connecting to node..
+    if (connectionStrength === -1) return 'Connecting..'
     if (connectionStrength === 0)
-      return 'Could not establish connection with our node.'
-    if (connectionStrength === 1) return 'Connected to an unhealthy node.'
-    if (connectionStrength === 2) return 'Node connected.'
+      // Could not establish connection with our node
+      return 'Disconnected'
+    // Connected to an unhealthy node
+    if (connectionStrength === 1) return 'Disconnected'
+    // Node connected
+    if (connectionStrength === 2) return 'Connected'
   }
 
   return (
-    <Box display='flex' flexDirection='row' alignItems='center' height='24px'>
+    <Box
+      display='inline-flex'
+      justifyContent='space-between'
+      alignItems='center'
+      minWidth={10}
+      p={2}
+      border={1}
+      borderColor='core.lightgray'
+      borderRadius={3}
+    >
       <ColoredDot connectionStrength={connectionStrength} />
-      <Text m={0} ml={3}>
+      <Text display='inline' textAlign='center' m={0} ml={3}>
         {nodeConnectedText()}
       </Text>
     </Box>
