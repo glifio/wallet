9f4b7140b40e52f770f04fd1f8c0ea052d417caa
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-07-22 13:23:13 -0400

    Update wallet down error text

--- pages/error/wallet-down.jsx
+++ pages/error/wallet-down.jsx
@@ -4,10 +4,10 @@ import { ErrorView } from '../../components/Shared'
 export default () => {
   return (
     <ErrorView
-      title='Glif is currently down'
-      description="We're aware of the outage and will be back up shortly."
-      linkDisplay='Follow @glifwallet for updates.'
-      linkhref='https://twitter.com/'
+      title='Oops! Something went wrong.'
+      description="We've been notified of the problem."
+      linkDisplay='Follow @openworklabs for updates.'
+      linkhref='https://twitter.com/openworklabs'
     />
   )
 }
