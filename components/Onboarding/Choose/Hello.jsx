d807f3cd996a7ca7bdd7258b43f54ffd501ac39a
Alex Singh <alex@alexsingh.com>
2020-07-22 11:57:26 -0300

    Add spacing

--- components/Onboarding/Choose/Hello.jsx
+++ components/Onboarding/Choose/Hello.jsx
@@ -5,7 +5,7 @@ import NodeConnectedWidget from '../../Shared/NodeConnected'
 export default ({ ...props }) => (
   <Box {...props}>
     <NodeConnectedWidget apiAddress={process.env.LOTUS_NODE_JSONRPC} />
-    <BigTitle>Glif</BigTitle>
+    <BigTitle mt={3}>Glif</BigTitle>
     <Text mb={1} maxWidth={12}>
       Create or login to your wallet to access the Filecoin network.
     </Text>
