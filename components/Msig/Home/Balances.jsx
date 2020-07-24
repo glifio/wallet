d0809d157e6a17d2ac5c9246d7d63700f4880170
Alex Singh <alex@alexsingh.com>
2020-07-24 11:49:33 -0300

    Create TabButton component. Setup state change.

--- components/Msig/Home/Balances.jsx
+++ components/Msig/Home/Balances.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
-import { Box, Label, Num } from '../../Shared'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Box, Label, Num, BaseButton } from '../../Shared'
 import { FILECOIN_NUMBER_PROP } from '../../../customPropTypes'
 import makeFriendlyBalance from '../../../utils/makeFriendlyBalance'
+
+const TabButton = styled(BaseButton)`
+  color: ${props => props.theme.colors.core.nearblack};
+  margin-left: ${props => props.theme.sizes[3]}px;
+  margin-right: ${props => props.theme.sizes[3]}px;
+  background: transparent;
+  align-content: center;
+  border: 1px solid;
+  border-radius: ${props => props.theme.radii[6]};
+  padding-top: ${props => props.theme.sizes[3]}px;
+  padding-bottom: ${props => props.theme.sizes[3]}px;
+  padding-left: ${props => props.theme.sizes[4]}px;
+  padding-right: ${props => props.theme.sizes[4]}px;
+  transition: 0.24s ease-in-out;
+
+  &:hover {
+    transform: translateY(-6%);
+  }
+
+  &:focus {
+    outline: 0;
+  }
+`
 
 const AvailableBalance = ({ available }) => {
   return (
@@ -12,9 +36,6 @@ const AvailableBalance = ({ available }) => {
       width='100%'
       p={4}
     >
-      <Label mb={1} textAlign='center'>
-        Available
-      </Label>
       <Num size='xxl' color='core.primary'>
         {makeFriendlyBalance(available, 6, true)}
       </Num>
@@ -34,9 +55,6 @@ const TotalBalance = ({ total }) => (
     width='100%'
     p={4}
   >
-    <Label mb={1} textAlign='center'>
-      Total Vesting
-    </Label>
     <Num size='xxl' color='core.primary'>
       {makeFriendlyBalance(total, 6, true)}
     </Num>
@@ -48,6 +66,8 @@ TotalBalance.propTypes = {
 }
 
 const Balances = ({ available, total }) => {
+  const [viewAvailable, setViewAvailable] = useState(true)
+
   return (
     <Box
       display='flex'
@@ -55,8 +75,19 @@ const Balances = ({ available, total }) => {
       alignItems='center'
       justifyContent='center'
     >
-      <AvailableBalance available={available} />
-      <TotalBalance total={total} />
+      <Box display='flex' flexGrow='1' justifyContent='space-between'>
+        <TabButton onClick={() => setViewAvailable(!viewAvailable)}>
+          Available
+        </TabButton>
+        <TabButton onClick={() => setViewAvailable(!viewAvailable)}>
+          Total Vesting
+        </TabButton>
+      </Box>
+      {viewAvailable ? (
+        <AvailableBalance available={available} />
+      ) : (
+        <TotalBalance total={total} />
+      )}
     </Box>
   )
 }
