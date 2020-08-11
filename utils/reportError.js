5943fc27b714462ed50ae4a99201d9c46b63ddcb
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-08-11 17:06:30 -0400

    TOB-FCW-001 - do not send error messages to slack

--- utils/reportError.js
+++ utils/reportError.js
@@ -2,17 +2,11 @@ import axios from 'axios'
 import Router from 'next/router'
 
 // This simply formats error messages and sends them to our slack channel
-export default async (id, shouldSendToErrorPage, ...args) => {
-  const errorText = args.reduce(
-    (err, ele) => {
-      return `${err}\n${ele}`
-    },
-    [`WALLET:${id}\n`]
-  )
+export default async (id, shouldSendToErrorPage) => {
   if (process.env.IS_PROD) {
     await axios.post(
       'https://errors.glif.io',
-      JSON.stringify({ text: errorText })
+      JSON.stringify({ text: `WALLET:${id}` })
     )
   }
   if (shouldSendToErrorPage) Router.push('/error/wallet-down')
