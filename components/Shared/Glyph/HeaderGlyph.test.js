25d48206611934ee56077c08e88f95053998eecc
Alex Singh <alex@alexsingh.com>
2020-08-27 14:09:48 -0300

    Update HeaderGlyph test

--- components/Shared/Glyph/HeaderGlyph.test.js
+++ components/Shared/Glyph/HeaderGlyph.test.js
@@ -1,86 +1,22 @@
-import { cleanup, render, act, fireEvent } from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
 import composeMockAppTree from '../../../test-utils/composeMockAppTree'
-import Glyph from '.'
-import theme from '../theme'
+import HeaderGlyph from './HeaderGlyph'
 
-describe('Glyph', () => {
+describe('HeaderGlyph', () => {
   afterEach(cleanup)
   test('renders the HeaderGlyph', () => {
     const { Tree } = composeMockAppTree('preOnboard')
     const { container } = render(
       <Tree>
-        <Box
-          display='flex'
-          alignItems='center'
-          justifyContent='space-between'
-          py={2}
-          mr={[0, 6]}
-          mb={[3, 0]}
-          px={3}
-          borderRadius={4}
-          width={['100%', 'auto']}
-          height='120px'
-          css={`
-            background: url(${imageUrl}) center no-repeat;
-            background-size: 100%;
-            border-radius: 16px;
-            alt: ${alt};
-          `}
-        >
-          <IconGlif
-            fill='#fff'
-            size={7}
-            css={`
-              transform: rotate(-90deg);
-            `}
-          />
-          <Header color={color} ml={3}>
-            {text}
-          </Header>
-        </Box>
+        <HeaderGlyph
+          alt='Source: https://www.nontemporary.com/post/190437968500'
+          text='Wallet'
+          imageUrl='/imgwallet.png'
+          color='black'
+        />
       </Tree>
     )
-    expect(Box).toMatchSnapshot()
+    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByText('Wallet')).toBeInTheDocument()
   })
-
-  test('renders the HeaderGlyph with the text', () => {
-    const { Tree } = composeMockAppTree('preOnboard')
-    const { getByText } = render(
-      <Tree>
-        <Box
-          display='flex'
-          alignItems='center'
-          justifyContent='space-between'
-          py={2}
-          mr={[0, 6]}
-          mb={[3, 0]}
-          px={3}
-          borderRadius={4}
-          width={['100%', 'auto']}
-          height='120px'
-          css={`
-            background: url(${imageUrl}) center no-repeat;
-            background-size: 100%;
-            border-radius: 16px;
-            alt: ${alt};
-          `}
-        >
-          <IconGlif
-            fill='#fff'
-            size={7}
-            css={`
-              transform: rotate(-90deg);
-            `}
-          />
-          <Header color={color} ml={3}>
-            {text}
-          </Header>
-        </Box>
-      </Tree>
-    )
-
-    expect(getByText('Sw')).toBeTruthy()
-    expect(Box).toMatchSnapshot()
-  })
-
-  
+})
