14756f8459a88c379cf50560af866a0c4f00f3ca
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-08-28 13:36:55 -0400

    Fix network switching

--- utils/reportError.js
+++ utils/reportError.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Router from 'next/router'
 
 // This simply formats error messages and sends them to our slack channel
-export default async (id, shouldSendToErrorPage) => {
+export default async (id, shouldSendToErrorPage, ...args) => {
   if (process.env.IS_PROD) {
     await axios.post(
       'https://errors.glif.io',
@@ -10,4 +10,6 @@ export default async (id, shouldSendToErrorPage) => {
     )
   }
   if (shouldSendToErrorPage) Router.push('/error/wallet-down')
+  /* eslint-disable no-console */
+  if (!shouldSendToErrorPage) console.error(id, ...args)
 }
