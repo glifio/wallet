7b7debccd05559206c3e97adab88bc1a4cc7a106
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-09-03 11:30:15 -0400

    Stub the landing page in the vault

--- pages/vault/index.jsx
+++ pages/vault/index.jsx
@@ -1,6 +1,5 @@
 import Landing from '../../components/Investor/Landing'
-import InvestorOnboarding from '../../components/Investor/InvestorOnboard'
 
 export default () => {
-  return <InvestorOnboarding />
+  return <Landing />
 }
