d311f203bc96e59f4e48bfc0793a11e769b2b6cb
kumavis <kumavis@users.noreply.github.com>
2020-09-04 21:21:37 +0800

    Fix BIP44 path hardening (#499)

--- utils/createPath/index.js
+++ utils/createPath/index.js
@@ -3,5 +3,5 @@ import { MAINNET_PATH_CODE, TESTNET_PATH_CODE } from '../../constants'
 export default (networkCode, i) => {
   if (networkCode !== MAINNET_PATH_CODE && networkCode !== TESTNET_PATH_CODE)
     throw new Error('Invalid network code passed')
-  return `m/44'/${networkCode}'/0/0/${i}`
+  return `m/44'/${networkCode}'/0'/0/${i}`
 }
