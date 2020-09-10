ce12f1fbcce2224aca9eec23dbac8893ea7d69c7
Alex Singh <alex@alexsingh.com>
2020-09-10 15:27:34 -0300

    Add spacing between Send Card and progress buttons

--- components/Wallet/Send.js/index.jsx
+++ components/Wallet/Send.js/index.jsx
@@ -384,6 +384,7 @@ const Send = ({ close }) => {
             maxWidth={14}
             width={13}
             minWidth={12}
+            mt={2}
           >
             <Button
               title='Back'
