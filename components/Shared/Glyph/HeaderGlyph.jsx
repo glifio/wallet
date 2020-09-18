6a702d3b2d664ad48f61ff9ed290f32289fe185e
Alex Singh <alex@alexsingh.com>
2020-09-18 11:25:22 -0300

    Improve HeaderGlyph text legibility

--- components/Shared/Glyph/HeaderGlyph.jsx
+++ components/Shared/Glyph/HeaderGlyph.jsx
@@ -7,6 +7,7 @@ import { Header } from '../Typography'
 const HeaderGlyph = ({ alt, color, text, imageUrl }) => {
   return (
     <Box
+      position='relative'
       display='flex'
       alignItems='center'
       justifyContent='space-between'
@@ -18,20 +19,32 @@ const HeaderGlyph = ({ alt, color, text, imageUrl }) => {
       width={['100%', 'auto']}
       height='120px'
       css={`
-        background: url(${imageUrl}) center no-repeat;
-        background-size: 100%;
-        border-radius: 16px;
-        alt: ${alt};
+        &:before {
+          content: '';
+          position: absolute;
+
+          top: 0px;
+          right: 0px;
+          bottom: 0px;
+          left: 0px;
+          background-color: rgba(0, 0, 0, 0.25);
+          background: url(${imageUrl}) center no-repeat;
+          background-size: 100%;
+          border-radius: 16px;
+          opacity: 0.7;
+          z-index: -9;
+          alt: ${alt};
+        }
       `}
     >
       <IconGlif
-        fill='#fff'
+        fill='#000'
         size={7}
         css={`
           transform: rotate(-90deg);
         `}
       />
-      <Header color={color} ml={3}>
+      <Header color={color} ml={3} zIndex={3}>
         {text}
       </Header>
     </Box>
