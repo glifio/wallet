e048ff5e76d687b4ec47737e2cc120d85615ce76
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-10-08 23:26:52 -0400

    Use node 4

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = phase => {
       env: {
         // this api is configured to be load balanced across multiple nodes,
         // if a single node gets sick, it will get dropped and not accept requests
-        LOTUS_NODE_JSONRPC: 'https://node.glif.io/space02/lotus/rpc/v0',
+        LOTUS_NODE_JSONRPC: 'https://node.glif.io/space04/lotus/rpc/v0',
         MAGIC_STRING_ENDPOINT: 'https://glif-verifier.vercel.app/api/verify',
         IS_PROD: true
       }
