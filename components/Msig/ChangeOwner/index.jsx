30d0e64992a915667b4676fb8d3a1288382b3789
Alex Singh <alex@alexsingh.com>
2020-10-13 11:23:56 -0300

    update code to match send & withdraw flows

--- components/Msig/ChangeOwner/index.jsx
+++ components/Msig/ChangeOwner/index.jsx
@@ -16,6 +16,7 @@ import {
   Input,
   Text,
   IconLedger,
+  InlineBox,
   Form
 } from '../../Shared'
 import {
@@ -163,150 +164,161 @@ const ChangeOwner = ({ address, balance, close }) => {
 
   return (
     <>
-      <ButtonClose
-        role='button'
-        type='button'
-        position='fixed'
-        top='3'
-        right='3'
-        onClick={() => {
-          setAttemptingTx(false)
-          setUncaughtError('')
-          resetLedgerState()
-          close()
-        }}
-      />
-      <Form onSubmit={onSubmit}>
-        <Box
-          maxWidth={14}
-          width={13}
-          minWidth={12}
-          display='flex'
-          flexDirection='column'
-          justifyContent='space-between'
-        >
-          <Box>
-            {hasLedgerError({ ...ledger, otherError: uncaughtError }) && (
-              <ErrorCard
-                error={reportLedgerConfigError({
-                  ...ledger,
-                  otherError: uncaughtError
-                })}
-                reset={() => {
+      <Box display='flex' flexDirection='column' width='100%'>
+        <ButtonClose
+          role='button'
+          type='button'
+          justifySelf='flex-end'
+          marginLeft='auto'
+          onClick={() => {
+            setAttemptingTx(false)
+            setUncaughtError('')
+            resetLedgerState()
+            close()
+          }}
+        />
+        <Form onSubmit={onSubmit}>
+          <Box
+            maxWidth={13}
+            width='100%'
+            minWidth={11}
+            display='flex'
+            flexDirection='column'
+            justifyContent='flex-start'
+            flexGrow='1'
+          >
+            <Box>
+              {hasLedgerError({ ...ledger, otherError: uncaughtError }) && (
+                <ErrorCard
+                  error={reportLedgerConfigError({
+                    ...ledger,
+                    otherError: uncaughtError
+                  })}
+                  reset={() => {
+                    setAttemptingTx(false)
+                    setUncaughtError('')
+                    resetLedgerState()
+                    setStep(2)
+                  }}
+                />
+              )}
+              {attemptingTx && (
+                <ConfirmationCard
+                  loading={fetchingTxDetails || mPoolPushing}
+                  walletType={LEDGER}
+                  currentStep={4}
+                  totalSteps={4}
+                  msig
+                />
+              )}
+              {!attemptingTx &&
+                !hasLedgerError({ ...ledger, otherError: uncaughtError }) && (
+                  <>
+                    <StepHeader
+                      title='Change Ownership'
+                      currentStep={step}
+                      totalSteps={4}
+                      glyphAcronym='Ch'
+                    />
+                    <ChangeOwnerHeaderText step={step} />
+                  </>
+                )}
+              {step === 1 && <Preface />}
+              <Box boxShadow={2} borderRadius={4}>
+                {step > 1 && (
+                  <CardHeader
+                    address={address}
+                    balance={balance}
+                    customizingGas={false}
+                  />
+                )}
+                {step > 1 && (
+                  <>
+                    <Box width='100%' p={3} border={0} bg='background.screen'>
+                      <Input.Address
+                        label='New owner'
+                        value={toAddress}
+                        onChange={e => setToAddress(e.target.value)}
+                        error={toAddressError}
+                        disabled={step === 3}
+                        onFocus={() => {
+                          if (toAddressError) setToAddressError('')
+                        }}
+                      />
+                    </Box>
+                    {step > 2 && (
+                      <Box
+                        display='flex'
+                        flexDirection='row'
+                        justifyContent='space-between'
+                        width='100%'
+                        p={3}
+                        border={0}
+                        bg='background.screen'
+                      >
+                        <Box>
+                          <Text margin={0}>Transaction Fee</Text>
+                          <Box display='flex' alignItems='flex-start'>
+                            <Text margin={0} color='core.darkgray'>
+                              Paid via{' '}
+                            </Text>
+                            <InlineBox ml={1} mr={2}>
+                              <IconLedger size={4} />
+                            </InlineBox>
+                            <Text margin={0} color='core.darkgray'>
+                              {makeFriendlyBalance(wallet.balance, 6, true)} FIL
+                            </Text>
+                          </Box>
+                        </Box>
+                        <Box display='flex' flexDirection='row'>
+                          <Text ml={2} color='core.primary'>
+                            {'< 0.0001 FIL'}
+                          </Text>
+                        </Box>
+                      </Box>
+                    )}
+                  </>
+                )}
+              </Box>
+            </Box>
+            <Box
+              display='flex'
+              flex='1'
+              flexDirection='row'
+              justifyContent='space-between'
+              alignItems='flex-end'
+              margin='auto'
+              maxWidth={13}
+              width='100%'
+              minWidth={11}
+              maxHeight={12}
+              my={3}
+            >
+              <Button
+                title='Back'
+                variant='secondary'
+                onClick={() => {
                   setAttemptingTx(false)
                   setUncaughtError('')
                   resetLedgerState()
-                  setStep(2)
+                  if (step === 1) {
+                    close()
+                  } else {
+                    setStep(step - 1)
+                  }
                 }}
+                disabled={attemptingTx}
               />
-            )}
-            {attemptingTx && (
-              <ConfirmationCard
-                loading={fetchingTxDetails || mPoolPushing}
-                walletType={LEDGER}
-                currentStep={4}
-                totalSteps={4}
-                msig
+              <Button
+                variant='primary'
+                title='Next'
+                disabled={isSubmitBtnDisabled()}
+                type='submit'
               />
-            )}
-            {!attemptingTx &&
-              !hasLedgerError({ ...ledger, otherError: uncaughtError }) && (
-                <>
-                  <StepHeader
-                    title='Change Ownership'
-                    currentStep={step}
-                    totalSteps={4}
-                    glyphAcronym='Ch'
-                  />
-                  <ChangeOwnerHeaderText step={step} />
-                </>
-              )}
-            {step === 1 && <Preface />}
-            {step > 1 && (
-              <CardHeader
-                address={address}
-                balance={balance}
-                customizingGas={false}
-              />
-            )}
-            {step > 1 && (
-              <>
-                <Box width='100%' p={3} border={0} bg='background.screen'>
-                  <Input.Address
-                    label='New owner'
-                    value={toAddress}
-                    onChange={e => setToAddress(e.target.value)}
-                    error={toAddressError}
-                    disabled={step === 3}
-                    onFocus={() => {
-                      if (toAddressError) setToAddressError('')
-                    }}
-                  />
-                </Box>
-                {step > 2 && (
-                  <Box
-                    display='flex'
-                    flexDirection='row'
-                    justifyContent='space-between'
-                    width='100%'
-                    p={3}
-                    border={0}
-                    bg='background.screen'
-                  >
-                    <Box>
-                      <Text margin={0}>Transaction Fee</Text>
-                      <Text margin={0} color='core.darkgray'>
-                        Paid via <IconLedger />{' '}
-                        {makeFriendlyBalance(wallet.balance, 6, true)} FIL
-                      </Text>
-                    </Box>
-                    <Box display='flex' flexDirection='row'>
-                      <Text ml={2} color='core.primary'>
-                        {'< 0.0001 FIL'}
-                      </Text>
-                    </Box>
-                  </Box>
-                )}
-              </>
-            )}
+            </Box>
           </Box>
-          <Box
-            display='flex'
-            flexDirection='row'
-            justifyContent='space-between'
-            position='fixed'
-            bottom='3'
-            margin='auto'
-            left='0'
-            right='0'
-            maxWidth={14}
-            width={13}
-            minWidth={12}
-          >
-            <Button
-              title='Back'
-              variant='secondary'
-              onClick={() => {
-                setAttemptingTx(false)
-                setUncaughtError('')
-                resetLedgerState()
-                if (step === 1) {
-                  close()
-                } else {
-                  setStep(step - 1)
-                }
-              }}
-              disabled={attemptingTx}
-            />
-            <Button
-              variant='primary'
-              title='Next'
-              disabled={isSubmitBtnDisabled()}
-              type='submit'
-            />
-          </Box>
-        </Box>
-      </Form>
+        </Form>
+      </Box>
     </>
   )
 }
