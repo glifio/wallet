67ca3a89c755ed7a6d050e32648a36b971b7708b
Jon <Schwartz10@users.noreply.github.com>
2020-11-13 12:52:27 -0500

    UI/UX improvements (#707)

--- utils/msig/stateDiff.js
+++ utils/msig/stateDiff.js
@@ -1,13 +1,17 @@
-export default (prevState, nextState) => {
+import isequal from 'lodash.isequal'
+
+const stateDiff = (prevState, nextState) => {
   const balanceChanged =
     prevState.Balance.toString() !== nextState.Balance.toString()
   const availBalanceChanged =
     prevState.AvailableBalance.toString() !==
     nextState.AvailableBalance.toString()
 
-  const signersChanged = prevState.Signers.some((signer, i) => {
-    return nextState.Signers[i] !== signer
-  })
+  const signersChanged = prevState.Signers.some((signer, i) =>
+    isequal(signer, nextState.Signers[i])
+  )
 
   return balanceChanged || availBalanceChanged || signersChanged
 }
+
+export default stateDiff
