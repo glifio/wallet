17f20074cdcb097d25cb94f2ab921f666280b9c4
Alex Singh <alex@alexsingh.com>
2020-11-25 17:14:04 -0300

    Copy address component no longer wraps

--- components/Shared/Copy/index.jsx
+++ components/Shared/Copy/index.jsx
@@ -4,10 +4,14 @@ import { string } from 'prop-types'
 import Box from '../Box'
 import BaseButton from '../Button/BaseButton'
 import { IconCopyAccountAddress } from '../Icons'
-import { Label, Title as AccountAddress } from '../Typography'
+import { Label, Title} from '../Typography'
 import truncate from '../../../utils/truncateAddress'
 import copyToClipboard from '../../../utils/copyToClipboard'
 import { ADDRESS_PROPTYPE } from '../../../customPropTypes'
+
+const AccountAddress = styled(Title)`
+white-space: nowrap;
+`
 
 const Copy = styled(BaseButton)`
   /* !important is declared here to override BaseButton's opacity:0.8 on hover. The only instance of us using this declaration. */
@@ -42,8 +46,6 @@ export const CopyAddress = forwardRef(({ address, ...props }, ref) => {
         fontWeight={1}
         fontSize={3}
         margin={0}
-        overflow='hidden'
-        whiteSpace='nowrap'
       >
         {truncate(address)}
       </AccountAddress>
