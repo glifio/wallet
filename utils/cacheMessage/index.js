d7fcec4c7b2699f39aef5485c6074270400db2b4
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-04-02 10:40:06 -0400

    Properly cache and load cached messages

--- utils/cacheMessage/index.js
+++ utils/cacheMessage/index.js
@@ -8,7 +8,7 @@ function checkClientSide() {
 
 const PENDING_MESSAGE_CACHE_PATH = 'PENDING-MSGS'
 
-function getMessagesFromLS(address) {
+export function getMessagesFromCache(address) {
   checkClientSide()
   const messages = window.localStorage.getItem(
     `${address}/${PENDING_MESSAGE_CACHE_PATH}`
@@ -18,28 +18,9 @@ function getMessagesFromLS(address) {
   return []
 }
 
-/**
- *
- * The idea here is that messages are cached until they show up on filfox
- *
- * Messages that are on filfox are stored as "confirmed" in redux
- * So if a message is confirmed by filfox, remove it from the cache (this happens in the use transaction history hook)
- * If a message is pending in redux, do not remove it, but do not add duplicate
- */
-export function retrievePendingMsgsAndReconcileCache(address, messagesInRedux) {
-  checkClientSide()
-  const pendingMessages = difference(
-    messagesInRedux.pending,
-    getMessagesFromLS(address)
-  )
-
-  return difference(pendingMessages, messagesInRedux.confirmed)
-}
-
 export function setMessageInCache(address, message) {
-  console.log('setting')
   checkClientSide()
-  const messages = getMessagesFromLS(address)
+  const messages = getMessagesFromCache(address)
   window.localStorage.setItem(
     `${address}/${PENDING_MESSAGE_CACHE_PATH}`,
     JSON.stringify([message, ...messages])
