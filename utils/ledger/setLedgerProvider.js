a7359441573ea269a3c9c82e85f230511f2b4bbb
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-06-03 13:05:20 -0400

    Revert some changes

--- utils/ledger/setLedgerProvider.js
+++ utils/ledger/setLedgerProvider.js
@@ -31,7 +31,6 @@ export const setLedgerProvider = async (dispatch, LedgerProvider) => {
     dispatch(createWalletProvider(provider))
     return provider
   } catch (err) {
-    console.log('ERR', err)
     if (
       err.message &&
       err.message.includes('TRANSPORT NOT SUPPORTED BY DEVICE')
