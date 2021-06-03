9633101a67195436cd717d374b6ba38c0356ee80
listenaddress <tbobrien612@gmail.com>
2021-06-03 14:41:56 -0400

    Fix copy explaining Ledger support

--- pages/error/use-chrome.jsx
+++ pages/error/use-chrome.jsx
@@ -4,7 +4,7 @@ import { ErrorView } from '../../components/Shared'
 export default () => {
   return (
     <ErrorView
-      title='Ledger only supports Chrome'
+      title='Only Chrome has Ledger support'
       description='Please install Google Chrome to continue using your Ledger device, or choose an alternative setup option'
       linkDisplay='Install Google Chrome.'
       linkhref='https://www.google.com/chrome'
