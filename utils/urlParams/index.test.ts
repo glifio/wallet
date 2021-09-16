a5d0df0e8c7722213a79c1e3b109c4522dfc68ed
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-09-16 16:29:13 -0400

    Fix tests

--- utils/urlParams/index.test.ts
+++ utils/urlParams/index.test.ts
@@ -3,41 +3,40 @@ import { PAGE } from '../../constants'
 
 describe('generateRouteWithRequiredUrlParams', () => {
   test('it adds the required params when generating navigation urls', () => {
-    const route = generateRouteWithRequiredUrlParams({}, PAGE.MSIG_HOME)
+    const route = generateRouteWithRequiredUrlParams({
+      existingQParams: {},
+      pageUrl: PAGE.MSIG_HOME
+    })
     expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
     expect(route.includes('?network=f')).toBe(true)
   })
 
   test('it does not change the required params when generating navigation urls', () => {
-    const route = generateRouteWithRequiredUrlParams(
-      { network: 't' },
-      PAGE.MSIG_HOME
-    )
+    const route = generateRouteWithRequiredUrlParams({
+      existingQParams: { network: 't' },
+      pageUrl: PAGE.MSIG_HOME
+    })
 
     expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
     expect(route.includes('?network=t')).toBe(true)
   })
 
   test('it allows for adding new query params', () => {
-    const route = generateRouteWithRequiredUrlParams(
-      { network: 't' },
-      PAGE.MSIG_HOME,
-      {
-        newQueryParams: { test: 'value' }
-      }
-    )
+    const route = generateRouteWithRequiredUrlParams({
+      existingQParams: { network: 't' },
+      pageUrl: PAGE.MSIG_HOME,
+      newQueryParams: { test: 'value' }
+    })
 
     expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
     expect(route.includes('network=t')).toBe(true)
     expect(route.includes('test=value')).toBe(true)
 
-    const route2 = generateRouteWithRequiredUrlParams(
-      { network: 'f' },
-      PAGE.MSIG_HOME,
-      {
-        newQueryParams: { test: 'value' }
-      }
-    )
+    const route2 = generateRouteWithRequiredUrlParams({
+      existingQParams: { network: 'f' },
+      pageUrl: PAGE.MSIG_HOME,
+      newQueryParams: { test: 'value' }
+    })
 
     expect(route2.includes(PAGE.MSIG_HOME)).toBe(true)
     expect(route2.includes('network=f')).toBe(true)
@@ -45,19 +44,19 @@ describe('generateRouteWithRequiredUrlParams', () => {
   })
 
   test('it allows for adding computed paths', () => {
-    const route = generateRouteWithRequiredUrlParams(
-      { network: 't' },
-      PAGE.MSIG_HOME,
-      {
-        urlPathExtension: ['extension']
-      }
-    )
+    const route = generateRouteWithRequiredUrlParams({
+      existingQParams: { network: 't' },
+      pageUrl: PAGE.MSIG_HOME,
+      urlPathExtension: ['extension']
+    })
 
     expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
     expect(route.includes('network=t')).toBe(true)
     expect(route.includes('/extension')).toBe(true)
 
-    const route2 = generateRouteWithRequiredUrlParams({}, PAGE.MSIG_HOME, {
+    const route2 = generateRouteWithRequiredUrlParams({
+      existingQParams: {},
+      pageUrl: PAGE.MSIG_HOME,
       urlPathExtension: ['extension']
     })
 
@@ -67,17 +66,16 @@ describe('generateRouteWithRequiredUrlParams', () => {
   })
 
   test('it works for the most complex cases', () => {
-    const route = generateRouteWithRequiredUrlParams(
-      { network: 't', param2: 'kobe' },
-      PAGE.MSIG_HOME,
-      {
-        urlPathExtension: ['extension', 'second-extension'],
-        newQueryParams: {
-          test: 'value',
-          test2: 'thingy'
-        }
+    const route = generateRouteWithRequiredUrlParams({
+      existingQParams: { network: 't', param2: 'kobe' },
+      pageUrl: PAGE.MSIG_HOME,
+
+      urlPathExtension: ['extension', 'second-extension'],
+      newQueryParams: {
+        test: 'value',
+        test2: 'thingy'
       }
-    )
+    })
 
     expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
     expect(route.includes('network=t')).toBe(true)
