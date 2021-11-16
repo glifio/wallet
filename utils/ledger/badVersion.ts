aeb8e8d13417c8020c83c8945a2fc45cf4286d96
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-11-16 07:05:08 -0500

    Fix semantic version checker

--- utils/ledger/badVersion.ts
+++ utils/ledger/badVersion.ts
@@ -13,10 +13,32 @@ const badVersion = ({
   minor: number
   patch: number
 }): boolean => {
-  const validMajor = major >= LEDGER_VERSION_MAJOR
-  const validMinor = minor >= LEDGER_VERSION_MINOR
-  const validPatch = patch >= LEDGER_VERSION_PATCH
-  return !(validMajor && validMinor && validPatch)
+  const aboveMajor = major > LEDGER_VERSION_MAJOR
+  const aboveMinor = minor > LEDGER_VERSION_MINOR
+  const abovePatch = patch > LEDGER_VERSION_PATCH
+
+  const atMajor = major === LEDGER_VERSION_MAJOR
+  const atMinor = minor === LEDGER_VERSION_MINOR
+  const atPatch = patch === LEDGER_VERSION_PATCH
+
+  const belowMajor = major < LEDGER_VERSION_MAJOR
+  const belowMinor = minor < LEDGER_VERSION_MINOR
+  const belowPatch = patch < LEDGER_VERSION_PATCH
+
+  if (aboveMajor) return false
+  if (belowMajor) return true
+
+  if (atMajor) {
+    if (aboveMinor) return false
+    if (belowMinor) return true
+
+    if (atMinor) {
+      if (abovePatch || atPatch) return false
+      if (belowPatch) return true
+    }
+  }
+
+  return true
 }
 
 export default badVersion
