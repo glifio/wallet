3ac130325357321ce0bfc8003d3e62d47e9be2b2
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-12-10 14:37:43 -0500

    Fix tests

--- utils/copyToClipboard/index.test.js
+++ utils/copyToClipboard/index.test.js
@@ -1,11 +1,6 @@
 import copyToClipboard from '.'
 
 describe('copyToClipboard', () => {
-  // test('it returns a promise', () => {
-  //   const returnVal = copyToClipboard('text')
-  //   expect(returnVal instanceof Promise).toBe(true)
-  // })
-
   test('it calls the navigator.clipboard.writeText method with the passed text', async () => {
     const { navigator } = window
     const mockWriteText = jest.fn(() => Promise.resolve())
@@ -22,16 +17,16 @@ describe('copyToClipboard', () => {
     expect(mockWriteText).toHaveBeenCalled()
   })
 
-  // test('it calls the document.execCommand method with "copy" when navigator.clipboard is undefined', async () => {
-  //   const mockDocExec = jest.fn()
-  //   window.document.execCommand = mockDocExec
-  //   const { navigator } = window
-  //   const mockNavigator = {}
-  //   delete window.navigator
-  //   window.navigator = mockNavigator
-  //   await copyToClipboard('text')
-  //   expect(mockDocExec).toHaveBeenCalledWith('copy')
-  //   window.navigator = navigator
-  //   delete window.document.execCommand
-  // })
+  test('it calls the document.execCommand method with "copy" when navigator.clipboard is undefined', async () => {
+    const mockDocExec = jest.fn()
+    window.document.execCommand = mockDocExec
+    const { navigator } = window
+    const mockNavigator = {}
+    delete window.navigator
+    window.navigator = mockNavigator
+    await copyToClipboard('text')
+    expect(mockDocExec).toHaveBeenCalledWith('copy')
+    window.navigator = navigator
+    delete window.document.execCommand
+  })
 })
