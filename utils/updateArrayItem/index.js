18e18401bad34c368fe699f725aea94df0add464
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-01-12 11:08:18 -0500

    Delete unused pages and funcs

--- utils/updateArrayItem/index.js
+++
@@ -1,6 +0,0 @@
-const updateArrayItem = (array, idxToReplace, replacement) =>
-  array.map((original, index) =>
-    index === idxToReplace ? replacement : original
-  )
-
-export default updateArrayItem
