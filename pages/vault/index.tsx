8a339c2888a4bc090149a70018025c0b39294b87
Jon <Schwartz10@users.noreply.github.com>
2022-01-12 11:09:56 -0500

    Delete unused pages and funcs (#934)

--- pages/vault/index.tsx
+++
@@ -1,9 +0,0 @@
-import useDesktopBrowser from '../../lib/useDesktopBrowser'
-import { MsigOnboard } from '../../components/Msig'
-
-const Vault = () => {
-  useDesktopBrowser()
-  return <MsigOnboard />
-}
-
-export default Vault
