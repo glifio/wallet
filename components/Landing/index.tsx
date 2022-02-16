ea0c5d82a258faa0cb4cd0212186cc190bf9ac49
Thijs van Hoof <thijs.v.h@gmail.com>
2022-02-16 23:55:55 +0100

    change wallet link for explorer

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -48,7 +48,7 @@ export default function Landing() {
         <AppHeader
           homeUrl={process.env.NEXT_PUBLIC_HOME_URL}
           blogUrl={process.env.NEXT_PUBLIC_BLOG_URL}
-          walletUrl={process.env.NEXT_PUBLIC_WALLET_URL}
+          explorerUrl={process.env.NEXT_PUBLIC_EXPLORER_URL}
           safeUrl={process.env.NEXT_PUBLIC_SAFE_URL}
         />
         <LandingPageInner>
