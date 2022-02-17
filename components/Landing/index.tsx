528a317f9212b9d4bdba5e3417700d825b3b4529
Thijs van Hoof <thijs.v.h@gmail.com>
2022-02-17 23:23:29 +0100

    Use Page component on landing page

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -1,18 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import {
-  AppHeader,
   AppTile,
   Box,
   IconCaution,
-  Footer,
-  PhishingBanner,
-  LandingPageOuter,
-  LandingPageInner,
+  LandingPageWrapper,
   LandingPageAppTile,
   LandingPageContent,
   space,
   fontSize,
   P,
+  Page,
   isMobileOrTablet,
   theme,
   useNetworkName,
@@ -42,127 +39,120 @@ export default function Landing() {
   )
 
   return (
-    <>
-      <LandingPageOuter>
-        <PhishingBanner href='https://wallet.glif.io' />
-        <AppHeader
-          homeUrl={process.env.NEXT_PUBLIC_HOME_URL}
-          blogUrl={process.env.NEXT_PUBLIC_BLOG_URL}
-          explorerUrl={process.env.NEXT_PUBLIC_EXPLORER_URL}
-          safeUrl={process.env.NEXT_PUBLIC_SAFE_URL}
-        />
-        <LandingPageInner>
-          <LandingPageAppTile>
-            <AppTile
-              title={
-                networkName && networkName !== 'Mainnet'
-                  ? `Wallet (${networkName})`
-                  : 'Wallet'
-              }
-              description='A lightweight interface for sending Filecoin.'
-              imgSrc='/bg-sender.jpg'
-              imgSrcHover='/bg-sender-hover.jpg'
-              small={false}
-              large
-            />
-          </LandingPageAppTile>
-          <LandingPageContent>
-            {unsupportedDevice ? (
-              <TextBox style={{ background: theme.colors.core.primary }}>
-                <P
-                  css={`
-                    font-size: ${fontSize('large')};
-                    color: white;
-                  `}
+    <Page
+      phishingUrl='https://wallet.glif.io'
+      homeUrl={process.env.NEXT_PUBLIC_HOME_URL}
+      blogUrl={process.env.NEXT_PUBLIC_BLOG_URL}
+      explorerUrl={process.env.NEXT_PUBLIC_EXPLORER_URL}
+      safeUrl={process.env.NEXT_PUBLIC_SAFE_URL}
+    >
+      <LandingPageWrapper>
+        <LandingPageAppTile>
+          <AppTile
+            title={
+              networkName && networkName !== 'Mainnet'
+                ? `Wallet (${networkName})`
+                : 'Wallet'
+            }
+            description='A lightweight interface for sending Filecoin.'
+            imgSrc='/bg-sender.jpg'
+            imgSrcHover='/bg-sender-hover.jpg'
+            small={false}
+            large
+          />
+        </LandingPageAppTile>
+        <LandingPageContent>
+          {unsupportedDevice ? (
+            <TextBox style={{ background: theme.colors.core.primary }}>
+              <P
+                css={`
+                  font-size: ${fontSize('large')};
+                  color: white;
+                `}
+              >
+                We&apos;re sorry, the Glif Safe only supports desktop browsers
+                at the moment. Please come back on your computer!
+              </P>
+            </TextBox>
+          ) : (
+            <>
+              <h2>Connect</h2>
+              <Box
+                display='flex'
+                flexDirection='column'
+                width='100%'
+                css={`
+                  &:not(:first-child) {
+                    margin-top: ${space()};
+                  }
+                `}
+              >
+                <ConnectBtn large onClick={() => connect(PAGE.CONNECT_MM)}>
+                  MetaMask
+                </ConnectBtn>
+                <ConnectBtn large onClick={() => connect(PAGE.CONNECT_LEDGER)}>
+                  Ledger Device
+                </ConnectBtn>
+                <Caution>
+                  <IconCaution />
+                  <P>
+                    Burner Wallets (use with caution,{' '}
+                    <a
+                      href='https://blog.glif.io/burner-wallets/'
+                      target='_blank'
+                      rel='nooppener noreferrer'
+                    >
+                      read more
+                    </a>
+                    )
+                  </P>
+                </Caution>
+                <BurnerWallet
+                  large
+                  onClick={() => connect(PAGE.CONNECT_BURNER_CREATE_SEED)}
                 >
-                  We&apos;re sorry, the Glif Safe only supports desktop browsers
-                  at the moment. Please come back on your computer!
-                </P>
-              </TextBox>
-            ) : (
-              <>
-                <h2>Connect</h2>
-                <Box
-                  display='flex'
-                  flexDirection='column'
-                  width='100%'
-                  css={`
-                    &:not(:first-child) {
-                      margin-top: ${space()};
-                    }
-                  `}
+                  Create Seed Phrase
+                </BurnerWallet>
+                <BurnerWallet
+                  large
+                  onClick={() => connect(PAGE.CONNECT_BURNER_IMPORT_SEED)}
                 >
-                  <ConnectBtn large onClick={() => connect(PAGE.CONNECT_MM)}>
-                    MetaMask
-                  </ConnectBtn>
-                  <ConnectBtn
-                    large
-                    onClick={() => connect(PAGE.CONNECT_LEDGER)}
-                  >
-                    Ledger Device
-                  </ConnectBtn>
-                  <Caution>
-                    <IconCaution />
-                    <P>
-                      Burner Wallets (use with caution,{' '}
-                      <a
-                        href='https://blog.glif.io/burner-wallets/'
-                        target='_blank'
-                        rel='nooppener noreferrer'
-                      >
-                        read more
-                      </a>
-                      )
-                    </P>
-                  </Caution>
-                  <BurnerWallet
-                    large
-                    onClick={() => connect(PAGE.CONNECT_BURNER_CREATE_SEED)}
-                  >
-                    Create Seed Phrase
-                  </BurnerWallet>
-                  <BurnerWallet
-                    large
-                    onClick={() => connect(PAGE.CONNECT_BURNER_IMPORT_SEED)}
-                  >
-                    Import Seed Phrase
-                  </BurnerWallet>
-                  <BurnerWallet
-                    large
-                    onClick={() => connect(PAGE.CONNECT_BURNER_IMPORT_PK)}
-                  >
-                    Import Private Key
-                  </BurnerWallet>
-                </Box>
+                  Import Seed Phrase
+                </BurnerWallet>
+                <BurnerWallet
+                  large
+                  onClick={() => connect(PAGE.CONNECT_BURNER_IMPORT_PK)}
+                >
+                  Import Private Key
+                </BurnerWallet>
+              </Box>
 
-                <Box mt={6}>
-                  <P>
-                    Want to load this app directly from IPFS or Filecoin?
-                    <br />
-                    Check our{' '}
-                    <SmartLink href='https://github.com/glifio/wallet/releases'>
-                      release page
-                    </SmartLink>
-                  </P>
-                  <P>
-                    Need help?
-                    <br />
-                    Open a{' '}
-                    <SmartLink href='https://github.com/glifio/wallet/issues/new/choose'>
-                      GitHub issue
-                    </SmartLink>{' '}
-                    or hit us up on{' '}
-                    <SmartLink href='https://twitter.com/glifio'>
-                      Twitter
-                    </SmartLink>
-                  </P>
-                </Box>
-              </>
-            )}
-          </LandingPageContent>
-        </LandingPageInner>
-      </LandingPageOuter>
-      <Footer />
-    </>
+              <Box mt={6}>
+                <P>
+                  Want to load this app directly from IPFS or Filecoin?
+                  <br />
+                  Check our{' '}
+                  <SmartLink href='https://github.com/glifio/wallet/releases'>
+                    release page
+                  </SmartLink>
+                </P>
+                <P>
+                  Need help?
+                  <br />
+                  Open a{' '}
+                  <SmartLink href='https://github.com/glifio/wallet/issues/new/choose'>
+                    GitHub issue
+                  </SmartLink>{' '}
+                  or hit us up on{' '}
+                  <SmartLink href='https://twitter.com/glifio'>
+                    Twitter
+                  </SmartLink>
+                </P>
+              </Box>
+            </>
+          )}
+        </LandingPageContent>
+      </LandingPageWrapper>
+    </Page>
   )
 }
