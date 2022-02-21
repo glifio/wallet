ca5e2eb752bb616841d4d719c60cd02c0756bf7b
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-02-21 09:27:51 -0500

    Update deps
    Update next config

--- next.config.js
+++ next.config.js
@@ -28,16 +28,16 @@ module.exports = (phase) => {
     env: {
       NEXT_PUBLIC_GRAPH_API_URL:
         process.env.GRAPH_API_URL || 'graph.glif.host/query',
-      NEXT_PUBLIC_HOME_URL: process.env.HOME_HREF || 'https://glif.io',
-      NEXT_PUBLIC_BLOG_URL: process.env.BLOG_HREF || 'https://glif.io/blog',
+      NEXT_PUBLIC_HOME_URL: process.env.HOME_URL || 'https://glif.io',
+      NEXT_PUBLIC_BLOG_URL: process.env.BLOG_URL || 'https://glif.io/blog',
       NEXT_PUBLIC_WALLET_URL:
-        process.env.WALLET_HREF || 'https://calibration.wallet.glif.io',
+        process.env.WALLET_URL || 'https://calibration.wallet.glif.io',
       NEXT_PUBLIC_SAFE_URL:
-        process.env.SAFE_HREF || 'https://calibration.safe.glif.io',
+        process.env.SAFE_URL || 'https://calibration.safe.glif.io',
       NEXT_PUBLIC_EXPLORER_URL:
-        process.env.EXPLORER_HREF || 'https://calibration.explorer.glif.io',
+        process.env.EXPLORER_URL || 'https://calibration.explorer.glif.io',
       NEXT_PUBLIC_VERIFIER_URL:
-        process.env.VERIFIER_HREF || 'https://calibration.verify.glif.io',
+        process.env.VERIFIER_URL || 'https://calibration.verify.glif.io',
       NEXT_PUBLIC_SENTRY_DSN: process.env.SENTRY_DSN || '',
       NEXT_PUBLIC_SENTRY_ENV: process.env.SENTRY_ENV || '',
       NEXT_PUBLIC_NODE_STATUS_API_ADDRESS:
