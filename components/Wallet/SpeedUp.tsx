3c58c25da5f20f1d5347d9ad25d0457fa9f7b631
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-07 00:09:54 +0200

    spped up start

--- components/Wallet/SpeedUp.tsx
+++ components/Wallet/SpeedUp.tsx
@@ -1,8 +1,25 @@
+import { useRouter } from 'next/router'
+import { useWalletProvider } from '@glif/wallet-provider-react'
+import {
+  useGetMessage,
+  useGetReplaceMessageGasParams
+} from '@glif/react-components'
+
 const SpeedUp = () => {
-  return (
-    <form>
-    </form>
-  )
+  const router = useRouter()
+  const { walletProvider } = useWalletProvider()
+  const {
+    message,
+    loading: messageLoading,
+    error: messageError
+  } = useGetMessage(router.query.cid)
+  const {
+    gasParams,
+    loading: gasParamsLoading,
+    error: gasParamsError
+  } = useGetReplaceMessageGasParams(walletProvider, message)
+
+  return <form></form>
 }
 
 export default SpeedUp
