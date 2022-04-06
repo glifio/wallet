9f0ddb8fd3c873de78f637f7c343d5019083b692
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-07 00:09:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,7 +16,9 @@ const webpack = (config) => {
     'react-dom': path.resolve('./node_modules/react-dom'),
     next: path.resolve('./node_modules/next'),
     'styled-components': path.resolve('./node_modules/styled-components'),
-    '@glif/react-components': path.resolve('./node_modules/@glif/react-components')
+    '@glif/react-components': path.resolve(
+      './node_modules/@glif/react-components'
+    )
   }
 
   return adjustedConf
