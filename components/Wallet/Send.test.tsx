195e21c6b955f6fcc26167df3ca390ee6cc8b3dd
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-05 15:34:36 +0200

    Update Send.test.tsx

---
+++ components/Wallet/Send.test.tsx
@@ -0,0 +1,106 @@
+import {
+  cleanup,
+  render,
+  act,
+  fireEvent,
+  getByText,
+  getByRole,
+  getAllByRole,
+  RenderResult
+} from '@testing-library/react'
+import { FilecoinNumber, BigNumber } from '@glif/filecoin-number'
+import { Message } from '@glif/filecoin-message'
+
+import composeMockAppTree from '../../test-utils/composeMockAppTree'
+import { flushPromises } from '../../test-utils'
+import { Send } from './Send'
+
+const validAddress = 't1iuryu3ke2hewrcxp4ezhmr5cmfeq3wjhpxaucza'
+const validAmount = new FilecoinNumber(0.01, 'fil')
+
+jest.mock('@glif/filecoin-wallet-provider')
+
+describe('Send', () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+    jest.clearAllMocks()
+  })
+
+  afterEach(() => {
+    jest.clearAllTimers()
+    cleanup()
+  })
+
+  test('it allows a user to send a message', async () => {
+    const { Tree, walletProvider } = composeMockAppTree('postOnboard')
+    let result: RenderResult | null = null
+
+    await act(async () => {
+      result = render(
+        <Tree>
+          <Send />
+        </Tree>
+      )
+
+      // Get HTML elements
+      const header = getByRole(result.container, 'heading')
+      const [recipient, params] = getAllByRole(result.container, 'textbox')
+      const [amount, txFee] = getAllByRole(result.container, 'spinbutton')
+      const cancel = getByText(result.container, 'Cancel')
+      const send = getByText(result.container, 'Send')
+
+      // Check initial state
+      expect(header).toHaveTextContent('Send Filecoin')
+      expect(recipient).toHaveFocus()
+      expect(recipient).toHaveDisplayValue('')
+      expect(params).toHaveDisplayValue('')
+      expect(amount).toHaveDisplayValue('')
+      expect(txFee).toHaveDisplayValue('')
+      expect(txFee).toBeDisabled()
+      expect(cancel).toBeEnabled()
+      expect(send).toBeDisabled()
+
+      // Enter recipient
+      fireEvent.change(recipient, { target: { value: validAddress } })
+      recipient.blur()
+      await flushPromises()
+      expect(send).toBeDisabled()
+
+      // Enter amount
+      amount.focus()
+      fireEvent.change(amount, { target: { value: validAmount } })
+      amount.blur()
+      await flushPromises()
+      expect(txFee).not.toHaveDisplayValue('')
+      expect(txFee).toBeEnabled()
+      expect(send).toBeEnabled()
+
+      // Click send
+      fireEvent.click(send)
+      await flushPromises()
+    })
+
+    // Check wallet provider calls
+    expect(walletProvider.getNonce).toHaveBeenCalled()
+    expect(walletProvider.wallet.sign).toHaveBeenCalled()
+    expect(walletProvider.simulateMessage).toHaveBeenCalled()
+    expect(walletProvider.sendMessage).toHaveBeenCalled()
+
+    // Check message
+    const lotusMessage = walletProvider.wallet.sign.mock.calls[0][1]
+    const message = Message.fromLotusType(lotusMessage)
+    expect(message.to).toBe(validAddress)
+    expect(message.value instanceof BigNumber).toBe(true)
+    expect(message.value.isEqualTo(validAmount)).toBe(true)
+    expect(typeof message.method).toBe('number')
+    expect(message.method).toBe(0)
+    expect(typeof message.params).toBe('string')
+    expect(message.params).toBe('')
+    expect(message.gasPremium instanceof BigNumber).toBe(true)
+    expect(message.gasPremium.isGreaterThan(0)).toBe(true)
+    expect(message.gasFeeCap instanceof BigNumber).toBe(true)
+    expect(message.gasFeeCap.isGreaterThan(0)).toBe(true)
+    expect(typeof message.gasLimit).toBe('number')
+    expect(message.gasLimit > 0).toBe(true)
+  })
+})
