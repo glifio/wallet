057dcb48ae075fc53921b3f45f300cbc15cffc62
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-06 11:37:35 +0200

    Use createPath from react-components

--- test-utils/composeMockAppTree/createWalletProviderContextFuncs.js
+++ test-utils/composeMockAppTree/createWalletProviderContextFuncs.js
@@ -1,6 +1,6 @@
 import { FilecoinNumber } from '@glif/filecoin-number'
+import { createPath } from '@glif/react-components'
 import { TESTNET_PATH_CODE } from '../../constants'
-import createPath from '../../utils/createPath'
 
 import {
   setLoginOption,
