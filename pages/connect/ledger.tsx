053212d1284ecae911a7ef53d26b4c16f88affc0
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-06 10:26:44 +0200

    Remove useReset hook

--- pages/connect/ledger.tsx
+++ pages/connect/ledger.tsx
@@ -2,18 +2,18 @@ import React, { useCallback } from 'react'
 import {
   OneColumnCentered,
   useChromeDesktopBrowser,
+  useWalletProvider,
   ConnectWallet
 } from '@glif/react-components'
 import { useRouter } from 'next/router'
 import WalletPage from '../../components/WalletPage'
-import useReset from '../../utils/useReset'
 import { navigate } from '../../utils/urlParams'
 import { PAGE } from '../../constants'
 
 export default function ConnectLedger() {
   useChromeDesktopBrowser()
   const router = useRouter()
-  const resetState = useReset()
+  const { resetState } = useWalletProvider()
   const back = useCallback(() => {
     router.replace('/')
     resetState()
