1812e1744da46aa90ba87bc88c422504d32d08e7
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-23 13:17:56 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -7,10 +7,10 @@ import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import 'whatwg-fetch'
 
-process.env.NEXT_PUBLIC_LOTUS_NODE_JSONRPC =
-  'https://calibration.node.glif.io/rpc/v0'
-process.env.NEXT_PUBLIC_COIN_TYPE = 't'
 process.env.NEXT_PUBLIC_WALLET_URL = 'https://wallet-calibration.glif.link'
 process.env.NEXT_PUBLIC_SAFE_URL = 'https://safe-calibration.glif.link'
 process.env.NEXT_PUBLIC_EXPLORER_URL = 'https://explorer-calibration.glif.link'
 process.env.NEXT_PUBLIC_VERIFIER_URL = 'https://verify-calibration.glif.link'
+process.env.NEXT_PUBLIC_LOTUS_NODE_JSONRPC =
+  'https://calibration.node.glif.io/rpc/v0'
+process.env.NEXT_PUBLIC_COIN_TYPE = 't'
