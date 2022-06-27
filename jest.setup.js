998fbf8389b44e14f1ed8ac83fefc41bea975b2d
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-27 14:33:30 +0200

    Add urls to jest config js

--- jest.setup.js
+++ jest.setup.js
@@ -7,6 +7,8 @@ import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import 'whatwg-fetch'
 
+process.env.NEXT_PUBLIC_HOME_URL = 'https://glif.io'
+process.env.NEXT_PUBLIC_BLOG_URL = 'https://blog.glif.io/'
 process.env.NEXT_PUBLIC_WALLET_URL = 'https://wallet-calibration.glif.link'
 process.env.NEXT_PUBLIC_SAFE_URL = 'https://safe-calibration.glif.link'
 process.env.NEXT_PUBLIC_EXPLORER_URL = 'https://explorer-calibration.glif.link'
