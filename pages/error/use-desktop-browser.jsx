df2f31ae7a7e5dd0257aee9e21cf3da1e7f1ca6b
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-27 14:34:16 +0200

    Update use-desktop-browser.jsx

--- pages/error/use-desktop-browser.jsx
+++ pages/error/use-desktop-browser.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { ButtonV2Link, ShadowBox, Dialog, ButtonRowCenter } from '@glif/react-components'
+import {
+  ButtonV2Link,
+  ShadowBox,
+  Dialog,
+  ButtonRowCenter
+} from '@glif/react-components'
 import { OneColumnCentered } from '@glif/react-components'
 import WalletPage from '../../components/WalletPage'
 
@@ -11,15 +16,13 @@ const UseDesktopBrowser = () => {
           <ShadowBox>
             <h2>Not yet</h2>
             <hr />
-            <p>
-              Glif Wallet isn&rsquo;t ready for your phone or tablet.
-            </p>
-            <p>
-            Please access it from your computer instead.
-            </p>
+            <p>Glif Wallet isn&rsquo;t ready for your phone or tablet.</p>
+            <p>Please access it from your computer instead.</p>
           </ShadowBox>
           <ButtonRowCenter>
-            <ButtonV2Link large green href={process.env.NEXT_PUBLIC_HOME_URL}>Home</ButtonV2Link>
+            <ButtonV2Link large green href={process.env.NEXT_PUBLIC_HOME_URL}>
+              Home
+            </ButtonV2Link>
           </ButtonRowCenter>
         </Dialog>
       </OneColumnCentered>
