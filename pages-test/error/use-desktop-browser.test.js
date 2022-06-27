e8ebcb8beb54921bfe6d911c223b8de7babb17d1
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-27 14:41:15 +0200

    Update use-desktop-browser.test.js

--- pages-test/error/use-desktop-browser.test.js
+++ pages-test/error/use-desktop-browser.test.js
@@ -13,42 +13,15 @@ describe('UseDesktopBrowser', () => {
         <UseDesktopBrowser />
       </Tree>
     )
-
-    expect(container.firstChild).toMatchSnapshot()
-  })
-
-  test('it renders the message about getting Chrome', () => {
-    const { Tree } = composeMockAppTree('postOnboard')
-
-    render(
-      <Tree>
-        <UseDesktopBrowser />
-      </Tree>
-    )
-
+    
     expect(
       screen.getByText('Glif Wallet isn’t ready for your phone or tablet.')
     ).toBeInTheDocument()
-  })
 
-  test('it sends the user to glif home page after clicking Home', async () => {
-    const { Tree } = composeMockAppTree('preOnboard')
-    delete window.location
+    expect(
+      screen.getByText('Home')
+    ).toHaveAttribute('href', 'https://glif.io')
 
-    window.location = {
-      href: ''
-    }
-
-    await act(async () => {
-      render(
-        <Tree>
-          <UseDesktopBrowser />
-        </Tree>
-      )
-      fireEvent.click(screen.getByText('Home'))
-      jest.runAllTimers()
-    })
-
-    expect(window.location.href).toBe('https://www.glif.io')
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
