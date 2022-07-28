7dc7690a53c62e1695b0303b9daa08a20e1f7ee0
Jon <Schwartz10@users.noreply.github.com>
2022-07-28 01:40:09 -0400

    change calib node (#1092)

--- jest.setup.js
+++ jest.setup.js
@@ -14,5 +14,5 @@ process.env.NEXT_PUBLIC_SAFE_URL = 'https://safe-calibration.glif.link'
 process.env.NEXT_PUBLIC_EXPLORER_URL = 'https://explorer-calibration.glif.link'
 process.env.NEXT_PUBLIC_VERIFIER_URL = 'https://verify-calibration.glif.link'
 process.env.NEXT_PUBLIC_LOTUS_NODE_JSONRPC =
-  'https://calibration.node.glif.io/rpc/v0'
+  'https://api.calibration.node.glif.io//rpc/v0'
 process.env.NEXT_PUBLIC_COIN_TYPE = 't'
