7f0e22076b9067f53f319c69fcea408d03018616
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-09-05 12:07:26 -0700

    Update rc
    Remove jest env vars

--- jest.setup.js
+++ jest.setup.js
@@ -6,11 +6,3 @@ global.TextEncoder = TextEncoder
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import 'whatwg-fetch'
-
-process.env.NEXT_PUBLIC_HOME_URL = 'https://glif.io'
-process.env.NEXT_PUBLIC_BLOG_URL = 'https://blog.glif.io/'
-process.env.NEXT_PUBLIC_WALLET_URL = 'https://wallet-calibration.glif.link'
-process.env.NEXT_PUBLIC_SAFE_URL = 'https://safe-calibration.glif.link'
-process.env.NEXT_PUBLIC_EXPLORER_URL = 'https://explorer-calibration.glif.link'
-process.env.NEXT_PUBLIC_VERIFIER_URL = 'https://verify-calibration.glif.link'
-process.env.NEXT_PUBLIC_COIN_TYPE = 't'
